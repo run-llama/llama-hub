294fefac4422e1a59402c89514188b749786da6f
EmptyCrown <jessetanzhang@gmail.com>
2023-02-01 19:02:16 -0800

    Add comment

--- loader_hub/__init__.py
+++ loader_hub/__init__.py
@@ -1,4 +1,4 @@
 from loader_hub.google_docs.base import GoogleDocsReader
-from loader_hub.file.base import SimpleDirectoryReader
+from loader_hub.file.base import SimpleDirectoryReader  # thejessezhang
 
 __all__ = ["GoogleDocsReader", "SimpleDirectoryReader"]
