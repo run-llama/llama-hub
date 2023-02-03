9e6434b3337ac4321e1d4b7504c28dafb1414ceb
EmptyCrown <jessetanzhang@gmail.com>
2023-02-02 21:35:56 -0800

    Library json

--- loader_hub/__init__.py
+++ loader_hub/__init__.py
@@ -1,15 +1,1 @@
-from loader_hub.google_docs.base import GoogleDocsReader
-from loader_hub.file.base import SimpleDirectoryReader
-from loader_hub.web.simple_web.base import SimpleWebPageReader
-from loader_hub.web.beautiful_soup_web.base import (
-    BeautifulSoupWebReader,
-)  # thejessezhang
-from loader_hub.web.trafilatura_web.base import TrafilaturaWebReader
-
-__all__ = [
-    "GoogleDocsReader",
-    "SimpleDirectoryReader",
-    "SimpleWebPageReader",
-    "BeautifulSoupWebReader",
-    "TrafilaturaWebReader",
-]
+"""Init file."""
