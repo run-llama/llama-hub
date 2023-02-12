0f82eb0e576bdca5c7035b1f8716d6568f1e07cd
ahmetkca <ahmet.karapinar@ontariotechu.net>
2023-02-12 04:01:39 -0500

    temp

--- loader_hub/github_repo/base.py
+++ loader_hub/github_repo/base.py
@@ -23,6 +23,7 @@ from typing import Any, Callable, List, Optional, Tuple
 from gpt_index.readers.base import BaseReader
 from gpt_index.readers.file.base import DEFAULT_FILE_EXTRACTOR
 
+# github raw usecontents api doesn't have the latest version of the file
 from modules.github_repo.github_client import (
     BaseGithubClient,
     GitBranchResponseModel,
@@ -31,6 +32,7 @@ from modules.github_repo.github_client import (
     GitTreeResponseModel,
 )
 
+# github raw usecontents api doesn't have the latest version of the file
 from modules.github_repo.utils import (
     BufferedGitBlobDataIterator,
     print_if_verbose,
