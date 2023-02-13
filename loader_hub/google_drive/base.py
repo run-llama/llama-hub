50577ada4fc4219f1f53cb92c79024f0518effa5
EmptyCrown <jessetanzhang@gmail.com>
2023-02-12 18:42:01 -0800

    Refactor google drive and address the google auth recurring

--- loader_hub/google_drive/base.py
+++ loader_hub/google_drive/base.py
@@ -1,22 +1,14 @@
 """Google Drive files reader."""
 
-from pathlib import Path
-from typing import Dict, List, Optional, Any, Union, Callable
-import os
 import logging
+import os
 import tempfile
+from pathlib import Path
+from typing import Any, List
 
 from gpt_index import download_loader
 from gpt_index.readers.base import BaseReader
 from gpt_index.readers.schema.base import Document
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-from googleapiclient.discovery import build
 
 logger = logging.getLogger(__name__)
 
@@ -27,35 +19,43 @@ SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]
 class GoogleDriveReader(BaseReader):
     """Google drive reader."""
 
-    def __init__(self,
-                 credentials_path: str = "credentials.json",
-                 client_secrets_path: str = "client_secrets.json",
-                 token_path: str = "token.json") -> None:
+    def __init__(
+        self,
+        credentials_path: str = "credentials.json",
+        token_path: str = "token.json",
+        pydrive_creds_path: str = "creds.txt",
+    ) -> None:
         """Initialize with parameters."""
         self.credentials_path = credentials_path
-        self.client_secrets_path = client_secrets_path
         self.token_path = token_path
+        self.pydrive_creds_path = pydrive_creds_path
+
+        self._creds = None
+        self._drive = None
 
     def _get_credentials(self) -> Any:
-        """
-        Reference: https://developers.google.com/drive/api/v3/quickstart/python
+        """Authenticate with Google and save credentials.
 
-        Download the credentials.json file with above reference.
-        Copy credentials.json file and rename it to client_secrets.json file which will be used by pydrive for downloading files.
-
-        So, we need two files:
-        1. credentials.json
-        2. client_secrets.json
-
-        Both 1, 2 are esentially same but needed with two different names according to google-api-python-client, google-auth-httplib2, google-auth-oauthlib and pydrive libraries.
+        Download the credentials.json file with these instructions: https://developers.google.com/drive/api/v3/quickstart/python.
+            Copy credentials.json file and rename it to client_secrets.json file which will be used by pydrive for downloading files.
+            So, we need two files:
+                1. credentials.json
+                2. client_secrets.json
+            Both 1, 2 are esentially same but needed with two different names according to google-api-python-client, google-auth-httplib2, google-auth-oauthlib and pydrive libraries.
 
         Returns:
             credentials, pydrive object
         """
+        from google.auth.transport.requests import Request
+        from google.oauth2.credentials import Credentials
+        from google_auth_oauthlib.flow import InstalledAppFlow
+        from pydrive.auth import GoogleAuth
+        from pydrive.drive import GoogleDrive
+
+        # First, we need the Google API credentials for the app
         creds = None
         if os.path.exists(self.token_path):
-            creds = Credentials.from_authorized_user_file(
-                self.token_path, SCOPES)
+            creds = Credentials.from_authorized_user_file(self.token_path, SCOPES)
         # If there are no (valid) credentials available, let the user log in.
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
@@ -69,15 +69,30 @@ class GoogleDriveReader(BaseReader):
             with open(self.token_path, "w") as token:
                 token.write(creds.to_json())
 
-        # Authorization for downloading files.
+        # Next, we need user authentication to download files (via pydrive)
         # Uses client_secrets.json file for authorization.
         gauth = GoogleAuth()
-        gauth.LocalWebserverAuth()
+        # Try to load saved client credentials
+        gauth.LoadCredentialsFile(self.pydrive_creds_path)
+        if gauth.credentials is None:
+            # Authenticate if they're not there
+            gauth.LocalWebserverAuth()
+        elif gauth.access_token_expired:
+            # Refresh them if expired
+            gauth.Refresh()
+        else:
+            # Initialize the saved creds
+            gauth.Authorize()
+        # Save the current credentials to a file so user doesn't have to auth every time
+        gauth.SaveCredentialsFile(self.pydrive_creds_path)
+
         drive = GoogleDrive(gauth)
 
         return creds, drive
 
-    def _get_fileids_meta(self, folder_id: str = None, file_id: str = None) -> List[str]:
+    def _get_fileids_meta(
+        self, folder_id: str = None, file_id: str = None
+    ) -> List[str]:
         """Get file ids present in folder/ file id
         Args:
             folder_id: folder id of the folder in google drive.
@@ -85,35 +100,49 @@ class GoogleDriveReader(BaseReader):
         Returns:
             metadata: List of metadata of filde ids.
         """
-        try:
+        from googleapiclient.discovery import build
 
-            service = build("drive", "v3", credentials=creds)
+        try:
+            service = build("drive", "v3", credentials=self._creds)
             if folder_id:
                 fileids_meta = []
                 query = "'" + folder_id + "' in parents"
-                results = service.files().list(
-                    q=query, fields="*").execute()
+                results = service.files().list(q=query, fields="*").execute()
                 items = results.get("files", [])
                 for item in items:
-                    if item["mimeType"] == 'application/vnd.google-apps.folder':
+                    if item["mimeType"] == "application/vnd.google-apps.folder":
                         fileids_meta.extend(
-                            self._get_fileids_meta(folder_id=item["id"]))
+                            self._get_fileids_meta(folder_id=item["id"])
+                        )
                     else:
                         fileids_meta.append(
-                            (item["id"], item["owners"][0]['displayName'], item["name"], item["createdTime"], item["modifiedTime"]))
+                            (
+                                item["id"],
+                                item["owners"][0]["displayName"],
+                                item["name"],
+                                item["createdTime"],
+                                item["modifiedTime"],
+                            )
+                        )
             else:
                 # Get the file details
                 file = service.files().get(fileId=file_id, fields="*").execute()
 
                 # Get metadata of the file
-                fileids_meta = (file["id"], file["owners"][0]['displayName'],
-                                file["name"], file["createdTime"], file["modifiedTime"])
+                fileids_meta = (
+                    file["id"],
+                    file["owners"][0]["displayName"],
+                    file["name"],
+                    file["createdTime"],
+                    file["modifiedTime"],
+                )
 
             return fileids_meta
 
         except Exception as e:
             logger.error(
-                'An error occurred while getting fileids metadata: {}'.format(e))
+                "An error occurred while getting fileids metadata: {}".format(e)
+            )
 
     def _download_file(self, fileid: str, filename: str) -> None:
         """Download the file with fileid and filename
@@ -124,14 +153,13 @@ class GoogleDriveReader(BaseReader):
             None
         """
         try:
-            file = drive.CreateFile({'id': fileid})
+            file = self._drive.CreateFile({"id": fileid})
             # download file with filename
             file.GetContentFile(filename)
         except Exception as e:
-            logger.error(
-                'An error occurred while downloading file: {}'.format(e))
+            logger.error("An error occurred while downloading file: {}".format(e))
 
-    def _load_data_fileids_meta(self, fileids_meta: str) -> List[Document]:
+    def _load_data_fileids_meta(self, fileids_meta: List[str]) -> List[Document]:
         """Load data from fileids metadata
         Args:
             fileids_meta: metadata of fileids in google drive.
@@ -141,6 +169,7 @@ class GoogleDriveReader(BaseReader):
 
         try:
             with tempfile.TemporaryDirectory() as temp_dir:
+
                 def get_metadata(filename):
                     return metadata[filename]
 
@@ -148,26 +177,25 @@ class GoogleDriveReader(BaseReader):
                 metadata = {}
 
                 for fileid_meta in fileids_meta:
-                    filename = fileid_meta[2]
+                    filename = next(tempfile._get_candidate_names())
                     filepath = os.path.join(temp_dir, filename)
                     fileid = fileid_meta[0]
                     self._download_file(fileid, filepath)
                     metadata[filepath] = {
                         "author": fileid_meta[1],
-                        "filename": filename,
-                        "createdTime": fileid_meta[3],
-                        "modifiedTime": fileid_meta[4]
+                        "file name": fileid_meta[2],
+                        "created at": fileid_meta[3],
+                        "modified at": fileid_meta[4],
                     }
-                SimpleDirectoryReader = download_loader(
-                    "SimpleDirectoryReader")
-                loader = SimpleDirectoryReader(
-                    temp_dir, file_metadata=get_metadata)
+                SimpleDirectoryReader = download_loader("SimpleDirectoryReader")
+                loader = SimpleDirectoryReader(temp_dir, file_metadata=get_metadata)
                 documents = loader.load_data()
 
             return documents
         except Exception as e:
             logger.error(
-                'An error occurred while loading data from fileids meta: {}'.format(e))
+                "An error occurred while loading data from fileids meta: {}".format(e)
+            )
 
     def _load_from_file_ids(self, file_ids: List[str]) -> List[Document]:
         """Load data from file ids
@@ -185,8 +213,7 @@ class GoogleDriveReader(BaseReader):
 
             return documents
         except Exception as e:
-            logger.error(
-                'An error occurred while loading with fileid: {}'.format(e))
+            logger.error("An error occurred while loading with fileid: {}".format(e))
 
     def _load_from_folder(self, folder_id: str) -> List[Document]:
         """Load data from folder_id
@@ -200,10 +227,11 @@ class GoogleDriveReader(BaseReader):
             documents = self._load_data_fileids_meta(fileids_meta)
             return documents
         except Exception as e:
-            logger.error(
-                'An error occurred while loading from folder: {}'.format(e))
+            logger.error("An error occurred while loading from folder: {}".format(e))
 
-    def load_data(self, folder_id: str = None, file_ids: List[str] = None) -> List[Document]:
+    def load_data(
+        self, folder_id: str = None, file_ids: List[str] = None
+    ) -> List[Document]:
         """Load data from the folder id and file ids.
         Args:
             folder_id: folder id of the folder in google drive.
@@ -211,8 +239,7 @@ class GoogleDriveReader(BaseReader):
         Returns:
             List[Document]: A list of documents.
         """
-        global creds, drive
-        creds, drive = self._get_credentials()
+        self._creds, self._drive = self._get_credentials()
 
         if folder_id:
             return self._load_from_folder(folder_id)
