0670b69ff6e48b34de90745e281f7c156cbe494f
EmptyCrown <jessetanzhang@gmail.com>
2023-02-14 21:37:14 -0800

    Better tests including download_loader

--- tests/test_library.py
+++ tests/test_library.py
@@ -15,15 +15,24 @@ def test_library_matches() -> None:
         # make sure every entry has an "id" field
         assert "id" in entry
         entry_id = entry["id"]
-        entry_dir = hub_dir / entry_id
+
         # make sure the loader directory exists
+        entry_dir = hub_dir / entry_id
         assert entry_dir.exists()
-        entry_file = entry_dir / "base.py"
+
         # make sure that the loader file exists
+        entry_file = entry_dir / "base.py"
         assert entry_file.exists()
+
+        # make sure that the README file exists
+        readme_file = entry_dir / "README.md"
+        assert readme_file.exists()
 
         spec = util.spec_from_file_location("custom_loader", location=str(entry_file))
         if spec is None:
             raise ValueError(f"Could not find file: {str(entry_file)}.")
         module = util.module_from_spec(spec)
         spec.loader.exec_module(module)  # type: ignore
+
+        # make sure the specified class is in the loader file
+        assert hasattr(module, k)
