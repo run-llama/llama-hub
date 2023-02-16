3c497ac4300302c551d2bad337860fbae04f4bea
Jesse Zhang <jessetanzhang@gmail.com>
2023-02-15 17:18:21 -0800

    Better tests, including download_loader (#36)
    * Better tests including download_loader
    
    * Fix file name
    
    * Import loaders locally
    
    * Fix relative imports
    
    * Update sys path
    
    * Update sys path
    
    * Import path
    
    * Import path

--- tests/test_library.py
+++ tests/test_library.py
@@ -7,7 +7,6 @@ from pathlib import Path
 
 def test_library_matches() -> None:
     """Check that library.json corresponds to valid files."""
-    # TODO: add more validation checks
     hub_dir = Path(__file__).parent.parent / "loader_hub"
     library_path = hub_dir / "library.json"
     library_dict = json.load(open(library_path, "r"))
@@ -15,15 +14,24 @@ def test_library_matches() -> None:
         # make sure every entry has an "id" field
         assert "id" in entry
         entry_id = entry["id"]
-        entry_dir = hub_dir / entry_id
+
         # make sure the loader directory exists
+        entry_dir = hub_dir / entry_id
         assert entry_dir.exists()
-        entry_file = entry_dir / "base.py"
+
         # make sure that the loader file exists
+        entry_file = entry_dir / "base.py"
         assert entry_file.exists()
+
+        # make sure that the README file exists
+        readme_file = entry_dir / "README.md"
+        assert readme_file.exists()
 
         spec = util.spec_from_file_location("custom_loader", location=str(entry_file))
         if spec is None:
             raise ValueError(f"Could not find file: {str(entry_file)}.")
         module = util.module_from_spec(spec)
         spec.loader.exec_module(module)  # type: ignore
+
+        # make sure the specified class is in the loader file
+        assert hasattr(module, k)
