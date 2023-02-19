00dff499f4642a80af67b21cf9b90351e3898f3f
batmanscode <29989939+batmanscode@users.noreply.github.com>
2023-02-20 03:04:51 +0530

    Add Whatsapp loader (#48)
    * Whatsapp loader (#1)
    
    * create whatsapp loader
    
    * update readme
    
    * update base.py and readme
    
    added ":" in front of `author`
    
    * update readme to say what verbose does
    
    * use logging instead of print
    
    - update readme
    - add loader to `library.json`

---
+++ loader_hub/whatsapp/base.py
@@ -0,0 +1,55 @@
+"""Whatsapp chat data loader"""
+
+import logging
+from pathlib import Path
+from typing import List
+
+from gpt_index.readers.base import BaseReader
+from gpt_index.readers.schema.base import Document
+
+
+class WhatsappChatLoader(BaseReader):
+    """
+    Whatsapp chat data loader.
+
+    Args:
+        path (str): Path to Whatsapp chat file.
+    """
+
+    def __init__(self, path: str):
+        """Initialize with path."""
+        
+        self.file_path = path
+
+    def load_data(self) -> List[Document]:
+        """
+        Parse Whatsapp file into Documents
+        """
+
+        import pandas as pd
+        from chatminer.chatparsers import WhatsAppParser
+
+        path = Path(self.file_path)
+
+        parser = WhatsAppParser(path)
+        parser.parse_file()
+        df = parser.parsed_messages.get_df()
+
+        logging.debug(f"> Number of messages: {len(df)}.")
+
+        docs = []
+        n = 0
+        for row in df.itertuples():
+            extra_info = {
+                "source": str(path).split("/")[-1].replace(".txt", ""), 
+                "author": row.author, 
+                "timestamp": str(row.timestamp)
+            }
+            
+            docs.append(Document(str(row.timestamp) + " " + row.author + ":" + " " + row.message, extra_info=extra_info))
+
+            n += 1
+            logging.debug(f"Added {n} of {len(df)} messages.")
+
+        logging.debug(f"> Document creation for {path} is complete.")
+        return docs
