01b00c0320480af65ea5b682f0e4bfb13089ae54
bubu <carr0t-bu@outlook.com>
2023-02-21 05:20:44 +0000

    fix error

--- loader_hub/memos/base.py
+++ loader_hub/memos/base.py
@@ -1,6 +1,6 @@
-"""Memos"""
+"""Simple Reader for Memos"""
 
-from typing import List, Optional
+from typing import List, Optional, Any
 
 from gpt_index.readers.base import BaseReader
 from gpt_index.readers.schema.base import Document
@@ -14,41 +14,39 @@ class MemosReader(BaseReader):
     """
 
     def __init__(self, host: str = "https://demo.usememos.com/") -> None:
-        """Initialize with parameters.
-        """
+        """Init params."""
         
         self._memoUrl = host + "api/memo"
 
-    def load_data(self, creator_id: Optional[int] = 101) -> List[Document]:
+    def load_data(self, params: Optional[Any] = 101) -> List[Document]:
         """Load data from RSS feeds.
 
         Args:
-            urls (List[str]): List of RSS URLs to load.
+            params (Any): Filtering parameters.
 
         Returns:
             List[Document]: List of documents.
 
         """
         import requests
-
+        
         documents = []
 
         realUrl = self._memoUrl
-        if creator_id:
-            realUrl += f'?creatorId={creator_id}&rowStatus=NORMAL'
-        else:
+        if ~params:
+            params = {}
             realUrl += "/all"
 
-        req = requests.get(realUrl)
+        req = requests.get(realUrl, params)
         
         if ~req.ok:
             return documents
         
         res = req.json()
+        
         memos = res["data"]
         
         for memo in memos:
-            
             content = memo["content"]
             extra_info = {"creator": memo["creator"], "resource_list": memo["resourceList"], id: memo["id"]}
             documents.append(Document(content, extra_info=extra_info))
