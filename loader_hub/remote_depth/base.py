a9638670ce0fd912ce116422fe87b3cb39611ec3
cdstrachan <craig@craigstrachan.com>
2023-03-15 14:02:40 +0200

    Update base.py

--- loader_hub/remote_depth/base.py
+++ loader_hub/remote_depth/base.py
@@ -46,7 +46,7 @@ class RemoteDepthReader(BaseReader):
                 print(link)
                 print(url)
                 """Checking if the link belongs the provided domain. """
-                if (self.domainlock && link>url):
+                if (self.domainlock and link>url):
                     if link in links_visited:
                         continue
                     if link:
