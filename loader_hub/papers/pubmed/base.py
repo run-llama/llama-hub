df26b14312b31fff9b76d54b5338196bf798bed9
EmptyCrown <jessetanzhang@gmail.com>
2023-03-18 15:00:35 -0700

    Fix pubmed

--- loader_hub/papers/pubmed/base.py
+++ loader_hub/papers/pubmed/base.py
@@ -11,12 +11,13 @@ class PubmedReader(BaseReader):
     Gets a search query, return a list of Documents of the top corresponding scientific papers on Pubmed.
     """
 
-    def load_data(
+    def load_data_bioc(
         self,
         search_query: str,
         max_results: Optional[int] = 10,
     ) -> List[Document]:
         """Search for a topic on Pubmed, fetch the text of the most relevant full-length papers.
+        Uses the BoiC API, which has been down a lot.
 
         Args:
             search_query (str): A topic to search for (e.g. "Alzheimers").
@@ -27,7 +28,7 @@ class PubmedReader(BaseReader):
         """
         from datetime import datetime
         import xml.etree.ElementTree as xml
-        
+
         import requests
 
         pubmed_search = []
@@ -86,5 +87,81 @@ class PubmedReader(BaseReader):
                         },
                     )
                 )
+
+        return pubmed_documents
+
+    def load_data(
+        self,
+        search_query: str,
+        max_results: Optional[int] = 10,
+    ) -> List[Document]:
+        """Search for a topic on Pubmed, fetch the text of the most relevant full-length papers.
+        Args:
+            search_query (str): A topic to search for (e.g. "Alzheimers").
+            max_results (Optional[int]): Maximum number of papers to fetch.
+        Returns:
+            List[Document]: A list of Document objects.
+        """
+        from datetime import datetime
+        import xml.etree.ElementTree as xml
+
+        import requests
+
+        pubmed_search = []
+        parameters = {"tool": "tool", "email": "email", "db": "pmc"}
+        parameters["term"] = search_query
+        parameters["retmax"] = max_results
+        resp = requests.get(
+            "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi",
+            params=parameters,
+        )
+        root = xml.fromstring(resp.content)
+
+        for elem in root.iter():
+            if elem.tag == "Id":
+                _id = elem.text
+                url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?id={_id}&db=pmc"
+                print(url)
+                try:
+                    resp = requests.get(url)
+                    info = xml.fromstring(resp.content)
+
+                    raw_text = ""
+                    title = ""
+                    journal = ""
+                    for element in info.iter():
+                        if element.tag == "article-title":
+                            title = element.text
+                        elif element.tag == "journal-title":
+                            journal = element.text
+
+                        if element.text:
+                            raw_text += element.text.strip() + " "
+
+                    pubmed_search.append(
+                        {
+                            "title": title,
+                            "journal": journal,
+                            "url": f"https://www.ncbi.nlm.nih.gov/pmc/articles/PMC{_id}/",
+                            "text": raw_text,
+                        }
+                    )
+                except Exception as e:
+                    print(e)
+                    pass
+
+        # Then get documents from Pubmed text, which includes abstracts
+        pubmed_documents = []
+        for paper in pubmed_search:
+            pubmed_documents.append(
+                Document(
+                    paper["text"],
+                    extra_info={
+                        "Title of this paper": paper["title"],
+                        "Journal it was published in:": paper["journal"],
+                        "URL": paper["url"],
+                    },
+                )
+            )
 
         return pubmed_documents
