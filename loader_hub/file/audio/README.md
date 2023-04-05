663c69af47086b49b5061081f098dd48a923fa89
ravi03071991 <ravi03071991@gmail.com>
2023-04-05 07:35:22 +0530

    Add transcription using gladia

--- loader_hub/file/audio/README.md
+++ loader_hub/file/audio/README.md
@@ -2,9 +2,21 @@
 
 This loader uses OpenAI's Whisper model to transcribe the text of an audio file or the audio track of a video file. The file formats .mp3 and .mp4 are preferred. A single local file is passed in each time you call `load_data`.
 
+You can directly use OpenAI's Whisper by setting `model_type` to `openai_whisper` or indirectly via Gladia OpenAI's Whisper by setting `model_type` to `openai_gladia_whisper` where the response time is usually less.
 ## Usage
 
 To use this loader, you need to pass in a `Path` to a local file.
+
+If you are using `openai_gladia_whisper`, please check following parameters on [Gladia](https://www.gladia.io/) before proceeding further.
+
+1. gladia_api_key
+2. diarization_max_speakers
+3. language
+4. language_behaviour
+5. target_translation_language
+6. transcription_hint
+
+You need to signup on [Gladia](https://www.gladia.io/) to get `API-KEY`
 
 ```python
 from pathlib import Path
--- loader_hub/file/audio/base.py
+++ loader_hub/file/audio/base.py
@@ -17,16 +17,32 @@ class AudioTranscriber(BaseReader):
 
     """
 
-    def __init__(self, *args: Any, model_version: str = "base", **kwargs: Any) -> None:
+    def __init__(self, *args: Any, model_version: str = "base", 
+                model_type: str = "openai_whisper", diarization_max_speakers: str = None,
+                language: str = "English", language_behaviour: str = "automatic multiple languages",
+                target_translation_language: str = 'english', gladia_api_key: str = None, 
+                transcription_hint: str = None, **kwargs: Any) -> None:
         """Init params."""
         super().__init__(*args, **kwargs)
-        self._model_version = model_version
 
-        import whisper
+        self.parser_config = {}
+        self.model_type = model_type
 
-        model = whisper.load_model(self._model_version)
+        if self.model_type == "openai_gladia_whsiper":
+            self.parser_config['gladia_api_key'] = gladia_api_key
+            self.parser_config['diarization_max_speakers'] = diarization_max_speakers
+            self.parser_config['language'] = language
+            self.parser_config['language_behaviour'] = language_behaviour
+            self.parser_config['target_translation_language'] = target_translation_language
+            self.parser_config['transcription_hint'] = transcription_hint
+        else:
+            self._model_version = model_version
 
-        self.parser_config = {"model": model}
+            import whisper
+
+            model = whisper.load_model(self._model_version)
+
+            self.parser_config = {"model": model}
 
     def load_data(
         self, file: Path, extra_info: Optional[Dict] = None
@@ -43,13 +59,41 @@ class AudioTranscriber(BaseReader):
             # Extract audio from video
             audio = video.split_to_mono()[0]
 
-            file_str = str(file)[:-4] + ".mp3"
+            file = str(file)[:-4] + ".mp3"
             # export file
-            audio.export(file_str, format="mp3")
+            audio.export(file, format="mp3")
 
-        model = cast(whisper.Whisper, self.parser_config["model"])
-        result = model.transcribe(str(file))
+        transcript = ""
 
-        transcript = result["text"]
+        if self.model_type == "openai_whisper":
+            model = cast(whisper.Whisper, self.parser_config["model"])
+            result = model.transcribe(str(file))
+
+            transcript = result["text"]
+        else:
+            import requests
+
+            headers = {
+                'accept': 'application/json',
+                'x-gladia-key': self.parser_config['gladia_api_key'],
+            }
+
+            files = {'audio': (str(file), open(str(file), 'rb'), 'audio/mpeg'),
+                     'output_format': (None, 'txt')}
+
+            if self.parser_config['diarization_max_speakers']:
+                files['diarization_max_speakers'] = (None, self.parser_config['diarization_max_speakers'])
+            if self.parser_config['language']:
+                files['language'] = self.parser_config['language']
+            if self.parser_config['language_behaviour']:
+                files['language_behaviour'] = self.parser_config['language_behaviour']
+            if self.parser_config['target_translation_language']:
+                files['target_translation_language'] = self.parser_config['target_translation_language']
+            if self.parser_config['transcription_hint']:
+                files = self.parser_config['transcription_hint']
+
+            response = requests.post('https://api.gladia.io/audio/text/audio-transcription/', headers=headers, files=files)
+            response_dict = response.json()
+            transcript = response_dict['prediction']
 
         return [Document(transcript, extra_info=extra_info)]
