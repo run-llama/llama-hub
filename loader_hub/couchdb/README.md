995f21d521caefa1fc144ec07d1a67d53a99c021
Matias Klein <matias.klein@kognition.ai>
2023-04-09 08:10:59 -0400

    couchdb loader

---
+++ loader_hub/couchdb/README.md
@@ -0,0 +1,27 @@
+# CouchDB Loader
+
+This loader loads documents from CouchDB. The user specifies a CouchDB instance to
+initialize the reader. They then specify the collection name and query params to
+fetch the relevant docs.
+
+## Usage
+
+Here's an example usage of the SimpleCouchDBReader.
+
+```python
+from llama_index import download_loader
+import os
+
+SimpleCouchDBReader = download_loader('SimpleCouchDBReader')
+
+host = "<host>"
+port = "<port>"
+db_name = "<db_name>"
+collection_name = "<collection_name>"
+# query_dict is passed into db.collection.find()
+query_dict = {}
+reader = SimpleCouchDBReader(host, port)
+documents = reader.load_data(db_name, collection_name, query_dict=query_dict)
+```
+
+This loader is designed to be used as a way to load data into [LlamaIndex](https://github.com/jerryjliu/gpt_index/tree/main/gpt_index) and/or subsequently used as a Tool in a [LangChain](https://github.com/hwchase17/langchain) Agent. See [here](https://github.com/emptycrown/llama-hub/tree/main) for examples.
---
+++ loader_hub/couchdb/base.py
@@ -0,0 +1,56 @@
+"""CouchDB client."""
+
+from typing import Dict, List, Optional
+
+from llama_index.readers.base import BaseReader
+from llama_index.readers.schema.base import Document
+
+
+class SimpleCouchDBReader(BaseReader):
+    """Simple CouchDB reader.
+
+    Concatenates each CouchDB doc into Document used by LlamaIndex.
+
+    Args:
+        couchdb_url (str): CouchDB Full URL.
+        max_docs (int): Maximum number of documents to load.
+
+    """
+
+    def __init__(self, user: str, pwd: str, host: str, port: int, couchdb_url: Optional[Dict] = None, max_docs: int = 1000) -> None:
+        """Initialize with parameters."""
+        import couchdb3;
+
+        if couchdb_url is not None:
+            self.client: CouchDBClient = couchdb3.Server(couchdb_url)
+        else:
+            self.client: CouchDBClient = couchdb3.Server(f'http://{user}:{pwd}@{host}:{port}')
+        self.max_docs = max_docs
+
+    def load_data(
+        self, db_name: str, collection_name: str, query_dict: Optional[Dict] = None
+    ) -> List[Document]:
+        """Load data from the input directory.
+
+        Args:
+            db_name (str): name of the database.
+            collection_name (str): name of the collection.
+            query_dict (Optional[Dict]): query to filter documents.
+                Defaults to None
+
+        Returns:
+            List[Document]: A list of documents.
+
+        """
+        documents = []
+        db = self.client.get(db_name)
+        if query_dict is None:
+            cursor = db.find()
+        else:
+            cursor = db.find(query_dict)
+
+        for item in cursor:
+            if "text" not in item:
+                raise ValueError("`text` field not found in CouchDB document.")
+            documents.append(Document(item["text"]))
+        return documents
