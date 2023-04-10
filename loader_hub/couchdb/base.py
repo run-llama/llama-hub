5f633a6c6f9c81a075e32ff995df8d52475c3f1a
Matias Klein <matias.klein@kognition.ai>
2023-04-10 08:01:37 -0400

    fix for id when returning multiple docs

--- loader_hub/couchdb/base.py
+++ loader_hub/couchdb/base.py
@@ -44,6 +44,7 @@ class SimpleCouchDBReader(BaseReader):
         documents = []
         db = self.client.get(db_name)
         if query is None:
+            #if no query is specified, return all docs in database
             logging.debug('showing all docs')
             results = db.all_docs()
         else:
@@ -58,13 +59,15 @@ class SimpleCouchDBReader(BaseReader):
         #check if more than one result
         if type(results) is not dict and results.rows is not None:
             for row in results.rows:
-                if "_id" not in row:
-                    raise ValueError("`_id` field not found in CouchDB document.")
+                #each doc has an id field
+                if "id" not in row:
+                    raise ValueError("`id` field not found in CouchDB document.")
                 documents.append(Document(json.dumps(row.value)))
         else:
             #only one result
             if results.get('docs') is not None:
                 for item in results.get('docs'):
+                    #check that the _id field exists
                     if "_id" not in item:
                         raise ValueError("`_id` field not found in CouchDB document.")
                     documents.append(Document(json.dumps(item)))
