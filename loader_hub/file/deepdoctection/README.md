410f233d9474accb350aa2b1e6b9d550f77f2e64
Jerry Liu <jerryjliu98@gmail.com>
2023-04-27 01:26:26 -0700

    add deepdoctection reader (#217)

---
+++ loader_hub/file/deepdoctection/README.md
@@ -0,0 +1,20 @@
+# DeepDoctection Loader
+
+This loader extracts the text from a local PDF file using the deepdoctection Python package, a library that performs
+doc extraction and document layout.
+
+## Usage
+
+To use this loader, you need to pass in a `Path` to a local file.
+
+```python
+from pathlib import Path
+from gpt_index import download_loader
+
+DeepDoctectionReader = download_loader("DeepDoctectionReader")
+
+loader = DeepDoctectionReader()
+documents = loader.load_data(file=Path('./article.pdf'))
+```
+
+This loader is designed to be used as a way to load data into [LlamaIndex](https://github.com/jerryjliu/gpt_index/tree/main/gpt_index) and/or subsequently used as a Tool in a [LangChain](https://github.com/hwchase17/langchain) Agent. See [here](https://github.com/emptycrown/llama-hub/tree/main) for examples.
---
+++ loader_hub/file/deepdoctection/base.py
@@ -0,0 +1,39 @@
+"""Deepdoctection Data Reader."""
+
+from pathlib import Path
+from typing import Optional, Set
+from typing import Dict, List
+from llama_index.readers.base import BaseReader
+from llama_index.readers.schema.base import Document
+
+
+class DeepDoctectionReader(BaseReader):
+    """Deepdoctection reader for pdf's.
+
+    Uses deepdoctection as a library to parse PDF files.
+
+    """
+
+    def __init__(self, attrs_as_metadata: Optional[Set] = None) -> None:
+        """Init params."""
+        import deepdoctection as dd
+        self.analyzer = dd.get_dd_analyzer()
+        self.attrs_as_metadata = attrs_as_metadata or set()
+
+    def load_data(
+        self, file: Path, extra_info: Optional[Dict] = None
+    ) -> List[Document]:
+        """Parse file."""
+        df = self.analyzer.analyze(path=str(file))
+        df.reset_state()
+        doc = iter(df)
+        result_docs = []
+        for page in doc:
+            doc_text = page.text
+            extra_info = {
+                k: getattr(page, k) for k in self.attrs_as_metadata if hasattr(page, k)
+            }
+            result_docs.append(
+                Document(doc_text, extra_info=extra_info)
+            )
+        return result_docs
