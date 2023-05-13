0d6167dd5b60083734d5941ef6c5ca79636556a1
Nadav Grinberg <nadavgr@monday.com>
2023-05-13 20:42:47 +0300

    Add monday loader

---
+++ loader_hub/mondaydotcom/README.md
@@ -0,0 +1,20 @@
+# Monday Loader
+
+This loader loads data from monday.com. The user specifies an API token to initialize the MondayReader. They then specify a GraphQL query to load in the corresponding Document objects.
+
+## Usage
+
+Here's an example usage of the MondayReader.
+
+```python
+from llama_index import download_loader
+import os
+
+MondayReader = download_loader('MondayReader')
+
+reader = MondayReader("<monday_api_token">)
+documents = reader.load_data(query="<GraphQL_QUERY>")
+
+```
+
+This loader is designed to be used as a way to load data into [LlamaIndex](https://github.com/jerryjliu/gpt_index/tree/main/gpt_index) and/or subsequently used as a Tool in a [LangChain](https://github.com/hwchase17/langchain) Agent. See [here](https://github.com/emptycrown/llama-hub/tree/main) for examples.
---
+++ loader_hub/mondaydotcom/base.py
@@ -0,0 +1,44 @@
+"""monday.com reader."""
+from typing import List
+
+from llama_index.readers.base import BaseReader
+from llama_index.readers.schema.base import Document
+
+
+class MondayReader(BaseReader):
+    """monday.com reader. Reads data by a GraphQL query.
+
+    Args:
+        api_key (str): monday.com API key.
+    """
+    
+    def __init__(self, api_key: str) -> None:
+        """Initialize monday.com reader."""
+        
+        self.api_key = api_key
+        self.api_url = "https://api.monday.com/v2"
+
+    def load_data(self, query: str) -> List[Document]:
+        """Load data by a graphQL query
+
+        Args:
+            query (str): GraphQL query.
+        Returns:
+            List[Document]: List of documents.
+        """
+        data = self.perform_request(query)
+        return [Document(f"{data}", extra_info={})]
+
+    def perform_request(query: str):
+        """Fetch GraphQL data"""
+
+        import requests
+        import json
+
+        headers = {"Authorization" : self.api_key}
+        data = {'query' : query}
+
+        response = requests.post(url=self.api_url, json=data, headers=headers)
+        json_response = r.json()
+        
+        return json_response
