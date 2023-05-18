eae82e23ad489de221f8bf9bed0f595c3695cc16
subhrajit <subhrajit2564@gmail.com>
2023-05-18 13:17:12 +0530

    Added oAuth to Jira loader

--- loader_hub/jira/README.md
+++ loader_hub/jira/README.md
@@ -1,6 +1,20 @@
 # JIRA Loader
 
-The Jira loader returns a set of issues based on the query provided to the dataloader. The user intializes the reader with an email, API token and the URL of the server they wish to fetch issues from.
+The Jira loader returns a set of issues based on the query provided to the dataloader.
+We can follow to methods to initialize the loader-
+1- basic_auth -> this takes a dict with the following keys
+`basic_auth:{
+"email": "email",
+"api_token": "token",
+"server_url": "server_url"
+}`
+2- Oauth2 -> this takes a dict with the following keys
+`oauth:{
+"cloud_id": "cloud_id",
+"api_token": "token"
+}`
+
+You can follow this link for more information regarding Oauth2 -> https://developer.atlassian.com/cloud/confluence/oauth-2-3lo-apps/
 
 ## Usage
 
--- loader_hub/jira/base.py
+++ loader_hub/jira/base.py
@@ -1,75 +1,104 @@
-from typing import List
+from typing import List, Optional, Dict, TypedDict
 
 from llama_index.readers.base import BaseReader
 from llama_index.readers.schema.base import Document
+
+
+class basic_auth(TypedDict):
+    email: str
+    api_token: str
+    server_url: str
+
+
+class oauth(TypedDict):
+    cloud_id: str
+    api_token: str
+
 
 class JiraReader(BaseReader):
     """Jira reader. Reads data from Jira issues from passed query.
 
     Args:
-        email (str): Jira email.
-        api_token (str): Jira API token.
-        server_url (str): Jira server url.
+        Optional basic_auth:{
+            "email": "email",
+            "api_token": "token",
+            "server_url": "server_url"
+        }
+        Optional oauth:{
+            "cloud_id": "cloud_id",
+            "api_token": "token"
+        }
     """
 
-    def __init__(self, email: str, api_token: str, server_url: str) -> None:
-
+    def __init__(
+        self, basic_auth: Optional[basic_auth] = None, oauth2: Optional[oauth] = None
+    ) -> None:
         from jira import JIRA
 
-        self.jira = JIRA(
-            basic_auth=(email, api_token),
-            server=f'https://{server_url}'
-        )
+        if oauth2:
+            options = {
+                "server": f"https://api.atlassian.com/ex/jira/{oauth2['cloud_id']}",
+                "headers": {"Authorization": f"Bearer {oauth2['api_token']}"},
+            }
+            self.jira = JIRA(options=options)
+        else:
+            self.jira = JIRA(
+                basic_auth=(basic_auth["email"], basic_auth["api_token"]),
+                server=f"https://{basic_auth['server_url']}",
+            )
 
     def load_data(self, query: str) -> List[Document]:
         relevant_issues = self.jira.search_issues(query)
 
         issues = []
 
+        assignee = ""
+        reporter = ""
+        epic_key = ""
+        epic_summary = ""
+        epic_descripton = ""
+
         for issue in relevant_issues:
             # Iterates through only issues and not epics
-            if 'parent' in (issue.raw['fields']):
-                assignee = ''
-                reporter = ''
-                epic_key = ''
-                epic_summary = ''
-                epic_descripton = ''
-
+            if "parent" in (issue.raw["fields"]):
                 if issue.fields.assignee:
                     assignee = issue.fields.assignee.displayName
 
                 if issue.fields.reporter:
                     reporter = issue.fields.reporter.displayName
 
-                if issue.raw['fields']['parent']['key']:
-                    epic_key = issue.raw['fields']['parent']['key']
-                
-                if issue.raw['fields']['parent']['fields']['summary']:
-                    epic_summary = issue.raw['fields']['parent']['fields']['summary']
-                
-                if issue.raw['fields']['parent']['fields']['status']['description']:
-                    epic_descripton = issue.raw['fields']['parent']['fields']['status']['description']
+                if issue.raw["fields"]["parent"]["key"]:
+                    epic_key = issue.raw["fields"]["parent"]["key"]
 
-                issues.append(
-                    Document(
-                        f"{issue.fields.summary} \n {issue.fields.description}",
-                        extra_info = {
-                            'id': issue.id,
-                            'title': issue.fields.summary,
-                            'url': issue.permalink(),
-                            'created_at': issue.fields.created,
-                            'updated_at': issue.fields.updated,
-                            'labels': issue.fields.labels,
-                            'status': issue.fields.status.name,
-                            'assignee': assignee,
-                            'reporter': reporter,
-                            'project': issue.fields.project.name,
-                            'issue_type': issue.fields.issuetype.name,
-                            'priority': issue.fields.priority.name,
-                            'epic_key': epic_key,
-                            'epic_summary': epic_summary,
-                            'epic_description': epic_descripton}
-                    )
+                if issue.raw["fields"]["parent"]["fields"]["summary"]:
+                    epic_summary = issue.raw["fields"]["parent"]["fields"]["summary"]
+
+                if issue.raw["fields"]["parent"]["fields"]["status"]["description"]:
+                    epic_descripton = issue.raw["fields"]["parent"]["fields"]["status"][
+                        "description"
+                    ]
+
+            issues.append(
+                Document(
+                    f"{issue.fields.summary} \n {issue.fields.description}",
+                    extra_info={
+                        "id": issue.id,
+                        "title": issue.fields.summary,
+                        "url": issue.permalink(),
+                        "created_at": issue.fields.created,
+                        "updated_at": issue.fields.updated,
+                        "labels": issue.fields.labels,
+                        "status": issue.fields.status.name,
+                        "assignee": assignee,
+                        "reporter": reporter,
+                        "project": issue.fields.project.name,
+                        "issue_type": issue.fields.issuetype.name,
+                        "priority": issue.fields.priority.name,
+                        "epic_key": epic_key,
+                        "epic_summary": epic_summary,
+                        "epic_description": epic_descripton,
+                    },
                 )
+            )
 
         return issues
