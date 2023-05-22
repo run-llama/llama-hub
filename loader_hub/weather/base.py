8a97ee85a54b16f04b0e479fd2ad7a3974db9ddc
Adheeban Manoharan <adheeban.m@saama.com>
2023-05-22 07:35:21 +0530

    enhancements to weather loader

--- loader_hub/weather/base.py
+++ loader_hub/weather/base.py
@@ -1,5 +1,5 @@
 """Simple reader that reads weather data from OpenWeatherMap API"""
-from typing import Any, List, Optional
+from typing import List
 
 from llama_index.readers.base import BaseReader
 from llama_index.readers.schema.base import Document
@@ -43,7 +43,7 @@ class WeatherReader(BaseReader):
         try:
             import pyowm
         except:
-            print('install pyowm using `pip install pyowm`')
+            raise ImportError('install pyowm using `pip install pyowm`')
 
         owm = pyowm.OWM(api_key=self.token)
         mgr = owm.weather_manager()
@@ -54,24 +54,33 @@ class WeatherReader(BaseReader):
         results = []
         for place in places:
             info_dict = {}
+            extra_info = {}
             list_of_locations = reg.locations_for(city_name=place)
-            city = list_of_locations[0]
+
+            try:
+                city = list_of_locations[0]
+            except:
+                raise ValueError(f"Unable to find {place}, try checking the spelling and try again")
             lat = city.lat
             lon = city.lon
     
             res = mgr.one_call(lat=lat,lon=lon)
             
+            
+            extra_info['latitude'] = lat
+            extra_info['longitude'] = lon
+            extra_info['timezone'] = res.timezone
             info_dict['location'] = place
-            info_dict['latitude'] = res.lat
-            info_dict['longitude'] = res.lon
-            info_dict['timezone'] = res.timezone
             info_dict['current weather'] = res.current.to_dict()
-            info_dict['daily forecast'] = [i.to_dict() for i in res.forecast_daily]
-            info_dict['hourly forecast'] = [i.to_dict() for i in res.forecast_hourly]
-            info_dict['minutely forecast'] = [i.to_dict() for i in res.forecast_minutely]
+            if res.forecast_daily:
+                info_dict['daily forecast'] = [i.to_dict() for i in res.forecast_daily]
+            if res.forecast_hourly:
+                info_dict['hourly forecast'] = [i.to_dict() for i in res.forecast_hourly]
+            if res.forecast_minutely:
+                info_dict['minutely forecast'] = [i.to_dict() for i in res.forecast_minutely]
             if res.national_weather_alerts:
                 info_dict['national weather alerts'] = [i.to_dict() for i in res.national_weather_alerts]
             
-            results.append(Document(info_dict))
+            results.append(Document(text=str(info_dict),extra_info=extra_info))
 
         return results
