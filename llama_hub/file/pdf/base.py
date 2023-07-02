9be5a46605e24662cb0f7dec9f6329ebecfd4d09
Alec Rooney <alec@adored.com>
2023-07-02 16:46:17 -0400

    allow a SpooledTemporaryFile to be passed in

--- llama_hub/file/pdf/base.py
+++ llama_hub/file/pdf/base.py
@@ -16,23 +16,22 @@ class PDFReader(BaseReader):
         """Parse file."""
         import pypdf
 
-        with open(file, "rb") as fp:
-            # Create a PDF object
-            pdf = pypdf.PdfReader(fp)
+        # Create a PDF object
+        pdf = pypdf.PdfReader(file)
 
-            # Get the number of pages in the PDF document
-            num_pages = len(pdf.pages)
+        # Get the number of pages in the PDF document
+        num_pages = len(pdf.pages)
 
-            # Iterate over every page
-            docs = []
-            for page in range(num_pages):
-                # Extract the text from the page
-                page_text = pdf.pages[page].extract_text()
-                page_label = pdf.page_labels[page]
-                metadata = {"page_label": page_label, "file_name": file.name}
+        # Iterate over every page
+        docs = []
+        for page in range(num_pages):
+            # Extract the text from the page
+            page_text = pdf.pages[page].extract_text()
+            page_label = pdf.page_labels[page]
+            metadata = {"page_label": page_label, "file_name": file.name}
 
-                if extra_info is not None:
-                    metadata.update(extra_info)
+            if extra_info is not None:
+                metadata.update(extra_info)
 
-                docs.append(Document(text=page_text, extra_info=metadata))
-            return docs
+            docs.append(Document(text=page_text, extra_info=metadata))
+        return docs
