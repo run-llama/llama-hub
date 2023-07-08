63ce041cfa9761c1a059840370403ac48de1621d
TuTu Solutions LLC <tutu.sol.jp@gmail.com>
2023-07-08 17:10:16 +0900

    [file/simple_csv] Add an argument encoding to specify the encoding to open the csv file.

--- llama_hub/file/simple_csv/base.py
+++ llama_hub/file/simple_csv/base.py
@@ -14,16 +14,21 @@ class SimpleCSVReader(BaseReader):
     """CSV parser.
 
     Args:
+        encoding (str): Encoding used to open the file.
+            utf-8 by default.
         concat_rows (bool): whether to concatenate all rows into one document.
             If set to False, a Document will be created for each row.
             True by default.
 
     """
 
-    def __init__(self, *args: Any, concat_rows: bool = True, **kwargs: Any) -> None:
+    def __init__(
+        self, *args: Any, concat_rows: bool = True, encoding: str = "utf-8", **kwargs: Any
+    ) -> None:
         """Init params."""
         super().__init__(*args, **kwargs)
         self._concat_rows = concat_rows
+        self._encoding = encoding
 
     def load_data(
         self, file: Path, extra_info: Optional[Dict] = None
@@ -32,7 +37,7 @@ class SimpleCSVReader(BaseReader):
         import csv
 
         text_list = []
-        with open(file, "r", encoding="utf-8") as fp:
+        with open(file, "r", encoding=self._encoding) as fp:
             csv_reader = csv.reader(fp)
             for row in csv_reader:
                 text_list.append(", ".join(row))
