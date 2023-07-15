cb1c9833731d82476b83b6c8961be795a02e815f
Adam Hofmann <ajhofmann18@gmail.com>
2023-07-15 17:25:10 +0300

    Simplify 'code-agent' to a code-tool'

--- llama_hub/tools/code_interpreter/README.md
+++ llama_hub/tools/code_interpreter/README.md
@@ -1,6 +1,6 @@
 # Code Interpreter Tool
 
-This tool can be used to run python scripts and capture the results of stdout
+This tool can be used to run python scripts and capture the results of stdout and stderr
 
 ## Usage
 
@@ -15,12 +15,12 @@ code_spec = CodeInterpreterToolSpec()
 agent = OpenAIAgent.from_tools(code_spec.to_tool_list())
 
 # Prime the agent to use the tool
-agent.chat('use generate_and_execute for the following tasks')
+agent.chat('Can you help me write some python code to pass to the code_interpreter tool')
 agent.chat('write a python function to calculate volume of a sphere with radius 4.3cm')
 ```
 
 The tools available are:
 
-`generate_and_execute`: A tool to evalute a python script
+`code_interpreter`: A tool to evalute a python script
 
 This loader is designed to be used as a way to load data as a Tool in a Agent. See [here](https://github.com/emptycrown/llama-hub/tree/main) for examples.
--- llama_hub/tools/code_interpreter/base.py
+++ llama_hub/tools/code_interpreter/base.py
@@ -9,73 +9,29 @@ import urllib.parse
 import sys
 import subprocess
 
-INITIAL_PROMPT = """
-    You are an AI assistant that is used to generate code.
-    When receiving natural language commands you will generate code to complete the required tasks.
-    The code that you generate should always be complete and runnable, as it will be executed immediately
-
-    You will ALWAYS write the function in python. You must import any libraries you use.
-
-    By default, the only response returned by the code is through stdout. Use print statements heavily.
-
-    Any code that is passed in as a prompt is NOT executed.
-
-    If you are requested to generate a graph, and want to use something like plt.show(), DONT. Instead save the output to a file.
-"""
-
-
 class CodeInterpreterToolSpec(BaseToolSpec):
     """Code Interpreter tool spec."""
 
-    spec_functions = ["generate_and_execute"]
+    spec_functions = ["code_interpreter"]
 
     def __init__(
         self,
         llm_cls: Optional[LLM] = OpenAI,
         llm_args: Optional[dict] = {}
     ) -> None:
-        self._llm = llm_cls(*llm_args)
-        self._initial_prompt = ChatMessage(role=MessageRole.SYSTEM, content=INITIAL_PROMPT)
-        self._prompts = []
-        self._responses = []
-
-    def _history(self) -> List[ChatMessage]:
-        return [msg for pair in zip(self._prompts, self._responses) for msg in pair]
-
-    def format_chat(self, prompt: ChatMessage) -> List[ChatMessage]:
-        return [self._initial_prompt, *self._history(), prompt]
-
-    def generate_code(self, prompt: str) -> str:
-        formatted_prompt = ChatMessage(role=MessageRole.USER, content=prompt)
-        response = self._llm.chat(self.format_chat(formatted_prompt))
-        self._prompts.append(formatted_prompt)
-        self._responses.append(ChatMessage(role=MessageRole.ASSISTANT, content=response.message.content))
-        return response.message.content
 
     def code_interpreter(self, code: str):
         """
         A function to execute python code, and return the stdout and stderr
+
+        You should import any libraries that you wish to use. You have access to any libraries the user has installed.
+
+        The code passed to this functuon is executed in isolation. It should be complete at the time it is passed to this function.
+
+        You should interpret the output and errors returned from this function, and attempt to fix any problems.
+        If you cannot fix the error, show the code to the user and ask for help
+
+        It is not possible to return graphics or other complicated data from this function. If the user cannot see the output, save it to a file and tell the user.
         """
         result = subprocess.run([sys.executable, '-c', code], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         return f'StdOut:\n{result.stdout}\nStdErr:\n{result.stderr}'
-
-    def generate_and_execute(self, prompt: str) -> str:
-        """
-        This tool generates python code and immediately executes the code.
-        Treat this tool as another Large Language Model Agent that will create the code for you, and execute it
-        This tool will also keep track of the previous calls you make for conveinence
-
-        An example input to this tool would be 'Calculate the quadratic formula at (0,1,2)'
-        or 'Analyze the csv at my_data.csv and tell me the average of a column'
-
-        Args:
-            prompt (str): the natural langauge prompt to generate code from
-
-        This function will return the code generated as well as the output
-        If the response contains stderr output, update the prompt to include the error and try again
-        """
-        code = self.generate_code(prompt)
-        result = self.code_interpreter(code)
-        return f'Code:\n{code}\n\n{result}'
-
-
