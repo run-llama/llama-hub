e4257a61b928a41ba880bfb62f12dfac9ebfc8d6
Simon Suo <simonsdsuo@gmail.com>
2023-07-17 18:03:17 -0700

    add tests and minor fixes

--- tests/test_library.py
+++ tests/test_library.py
@@ -38,3 +38,35 @@ def test_library_matches() -> None:
 
         # make sure the specified class is in the loader file
         assert hasattr(module, k)
+
+
+def test_tools_library_matches() -> None:
+    """Check that library.json corresponds to valid files."""
+    hub_dir = Path(__file__).parent.parent / "llama_hub"
+    library_path = hub_dir / 'tools' / "library.json"
+    library_dict = json.load(open(library_path, "r"))
+    for k, entry in library_dict.items():
+        # make sure every entry has an "id" field
+        assert "id" in entry
+        entry_id = entry["id"]
+
+        # make sure the tool directory exists
+        entry_dir = hub_dir / entry_id
+        assert entry_dir.exists()
+
+        # make sure that the tool file exists
+        entry_file = entry_dir / "base.py"
+        assert entry_file.exists()
+
+        # make sure that the README file exists
+        readme_file = entry_dir / "README.md"
+        assert readme_file.exists()
+
+        spec = util.spec_from_file_location("custom_tool", location=str(entry_file))
+        if spec is None:
+            raise ValueError(f"Could not find file: {str(entry_file)}.")
+        module = util.module_from_spec(spec)
+        spec.loader.exec_module(module)  # type: ignore
+
+        # make sure the specified class is in the loader file
+        assert hasattr(module, k)
