601efd6e28118709579ce21b3499d3c658c38bfb
Sadegh Ranjbar <s.ranjbar@dataak.com>
2023-09-09 23:18:33 +0330

    change elastic client to the official package

--- llama_hub/elasticsearch/base.py
+++ llama_hub/elasticsearch/base.py
@@ -4,11 +4,11 @@ This only uses the basic search api, so it will work with Elasticsearch and Open
 
 """
 
-
 from typing import List, Optional
 
 from llama_index.readers.base import BaseReader
 from llama_index.readers.schema.base import Document
+from elasticsearch import Elasticsearch
 
 
 class ElasticsearchReader(BaseReader):
@@ -18,26 +18,25 @@ class ElasticsearchReader(BaseReader):
     These documents can then be used in a downstream Llama Index data structure.
 
     Args:
-        endpoint (str): URL (http/https) of cluster
+        endpoint (str): URL (http/https) of cluster without port
         index (str): Name of the index (required)
-        httpx_client_args (dict): Optional additional args to pass to the `httpx.Client`
+        basic_auth (set): basic authentication username password
     """
 
     def __init__(
-        self, endpoint: str, index: str, httpx_client_args: Optional[dict] = None
+            self, endpoint: str, index: str, basic_auth: Optional[set] = None
     ):
         """Initialize with parameters."""
-        import httpx  # noqa: F401
 
-        self._client = httpx.Client(base_url=endpoint, **(httpx_client_args or {}))
+        self._es_client = Elasticsearch(endpoint, basic_auth=basic_auth)
         self._index = index
         self._endpoint = endpoint
 
     def load_data(
-        self,
-        field: str,
-        query: Optional[dict] = None,
-        embedding_field: Optional[str] = None,
+            self,
+            field: str,
+            query: Optional[dict] = None,
+            embedding_field: Optional[str] = None,
     ) -> List[Document]:
         """Read data from the Elasticsearch index.
 
@@ -45,7 +44,7 @@ class ElasticsearchReader(BaseReader):
             field (str): Field in the document to retrieve text from
             query (Optional[dict]): Elasticsearch JSON query DSL object.
                 For example:
-                {"query": {"match": {"message": {"query": "this is a test"}}}}
+                { "query" : {"match": {"message": {"query": "this is a test"}}}}
             embedding_field (Optional[str]): If there are embeddings stored in
                 this index, this field can be used
                 to set the embedding field on the returned Document list.
@@ -53,10 +52,12 @@ class ElasticsearchReader(BaseReader):
             List[Document]: A list of documents.
 
         """
-        res = self._client.post(f"{self._index}/_search", json=query).json()
+        query = query['query'] if query is not None else None  # To remain backward compatible
+        res = self._es_client.search(index=self._index, query=query)
         documents = []
         for hit in res["hits"]["hits"]:
             value = hit["_source"][field]
+            _ = hit['_source'].pop(field)
             embedding = hit["_source"].get(embedding_field or "", None)
             documents.append(
                 Document(text=value, extra_info=hit["_source"], embedding=embedding)
