9923d38094cf445d8aa4bbfd34e278ac3f50a5d9
Hossam Hammady <github@hammady.net>
2023-10-06 20:17:07 -0400

    Lint code

--- llama_hub/rayyan/base.py
+++ llama_hub/rayyan/base.py
@@ -10,7 +10,14 @@ from rayyan.user import User
 from rayyan.review import Review
 
 from tqdm import tqdm
-from tenacity import retry, stop_all, stop_after_attempt, stop_after_delay, wait_random_exponential
+from tenacity import (
+    retry,
+    stop_all,
+    stop_after_attempt,
+    stop_after_delay,
+    wait_random_exponential,
+)
+
 
 class RayyanReader(BaseReader):
     """Rayyan reader. Reads articles from a Rayyan review.
@@ -18,10 +25,12 @@ class RayyanReader(BaseReader):
     Args:
         credentials_path (str): Rayyan credentials path.
         rayyan_url (str, optional): Rayyan URL. Defaults to https://rayyan.ai.
-            Set to an alternative URL if you are using a non-production Rayyan instance. 
+            Set to an alternative URL if you are using a non-production Rayyan instance.
     """
 
-    def __init__(self, credentials_path: str, rayyan_url: str = 'https://rayyan.ai') -> None:
+    def __init__(
+        self, credentials_path: str, rayyan_url: str = "https://rayyan.ai"
+    ) -> None:
         """Initialize Rayyan reader."""
         logging.debug("Initializing Rayyan reader...")
         self.rayyan = Rayyan(credentials_path, url=rayyan_url)
@@ -41,12 +50,11 @@ class RayyanReader(BaseReader):
 
         rayyan_review = Review(self.rayyan)
         my_review = rayyan_review.get(review_id)
-        logging.info(f"Working on review: '{my_review['title']}' with {my_review['total_articles']} total articles.")
+        logging.info(
+            f"Working on review: '{my_review['title']}' with {my_review['total_articles']} total articles."
+        )
 
-        result_params = {
-            "start": 0,
-            "length": 100
-        }
+        result_params = {"start": 0, "length": 100}
         result_params.update(filters)
 
         @retry(
@@ -54,44 +62,43 @@ class RayyanReader(BaseReader):
             stop=stop_all(stop_after_attempt(3), stop_after_delay(30)),
         )
         def fetch_results_with_retry():
-          logging.debug("Fetch parameters: %s", result_params)
-          return rayyan_review.results(review_id, result_params)
-        
+            logging.debug("Fetch parameters: %s", result_params)
+            return rayyan_review.results(review_id, result_params)
+
         articles = []
-        logging.info(f"Fetching articles from Rayyan...")
+        logging.info("Fetching articles from Rayyan...")
         total = my_review["total_articles"]
         with tqdm(total=total) as pbar:
-          while len(articles) < total:
-            # retrieve articles in batches
-            review_results = fetch_results_with_retry()
-            fetched_articles = review_results["data"]
-            articles.extend(fetched_articles)
-            # update total in case filters are applied
-            if total != review_results["recordsFiltered"]:
-              total = review_results["recordsFiltered"]
-              pbar.total = total
-            result_params["start"] += len(fetched_articles)
-            pbar.update(len(fetched_articles))
+            while len(articles) < total:
+                # retrieve articles in batches
+                review_results = fetch_results_with_retry()
+                fetched_articles = review_results["data"]
+                articles.extend(fetched_articles)
+                # update total in case filters are applied
+                if total != review_results["recordsFiltered"]:
+                    total = review_results["recordsFiltered"]
+                    pbar.total = total
+                result_params["start"] += len(fetched_articles)
+                pbar.update(len(fetched_articles))
 
         results = []
         for article in articles:
             # iterate over all abstracts
             abstracts = ""
             if article["abstracts"] is not None:
-              abstracts_arr = [abstract["content"] for abstract in article["abstracts"]]
-              if len(abstracts_arr) > 0:
-                # map array into a string
-                abstracts = "\n".join(abstracts_arr)[0:1024].strip()
+                abstracts_arr = [
+                    abstract["content"] for abstract in article["abstracts"]
+                ]
+                if len(abstracts_arr) > 0:
+                    # map array into a string
+                    abstracts = "\n".join(abstracts_arr)[0:1024].strip()
             title = article["title"]
             if title is not None:
-              title = title.strip()
+                title = title.strip()
             body = f"{title}\n{abstracts}"
             if body.strip() == "":
                 continue
-            extra_info = {
-                "id": article["id"],
-                "title": title
-            }
+            extra_info = {"id": article["id"], "title": title}
 
             results.append(
                 Document(
