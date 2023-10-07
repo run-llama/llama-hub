a61700f616c5e37394114bff277676596361cfa6
Lejdi Prifti <lejdi.prifti@linksmt.it>
2023-10-07 10:43:42 +0200

    formatted

--- llama_hub/bitbucket/base.py
+++ llama_hub/bitbucket/base.py
@@ -14,102 +14,108 @@ class BitbucketReader(BaseReader):
     Reads the content of files in Bitbucket repositories.
 
     """
-    def __init__(
-          self,
-          base_url: Optional[str] = None,
-          project_key: Optional[str] = None,
-          branch: Optional[str] = 'refs/heads/develop',
-      ) -> None:
-          """Initialize with parameters."""
-          if os.getenv('BITBUCKET_USERNAME') is None:
-            raise ValueError("Could not find a Bitbucket username.")
-          if os.getenv('BITBUCKET_API_KEY') is None:
-            raise ValueError("Could not find a Bitbucket api key.")
-          if base_url is None:
-            raise ValueError("You must provide a base url for Bitbucket.")
-          if project_key is None:
-            raise ValueError("You must provide a project key for Bitbucket repository.")
-          self.base_url = base_url
-          self.project_key = project_key
-          self.branch = branch
 
+    def __init__(
+        self,
+        base_url: Optional[str] = None,
+        project_key: Optional[str] = None,
+        branch: Optional[str] = "refs/heads/develop",
+    ) -> None:
+        """Initialize with parameters."""
+        if os.getenv("BITBUCKET_USERNAME") is None:
+            raise ValueError("Could not find a Bitbucket username.")
+        if os.getenv("BITBUCKET_API_KEY") is None:
+            raise ValueError("Could not find a Bitbucket api key.")
+        if base_url is None:
+            raise ValueError("You must provide a base url for Bitbucket.")
+        if project_key is None:
+            raise ValueError("You must provide a project key for Bitbucket repository.")
+        self.base_url = base_url
+        self.project_key = project_key
+        self.branch = branch
 
     def get_slugs(self) -> List:
-      """
-      Get slugs of the specific project.
-      """
-      username = os.getenv('BITBUCKET_USERNAME')
-      api_token = os.getenv('BITBUCKET_API_KEY')
-      auth = base64.b64encode(f"{username}:{api_token}".encode()).decode()
+        """
+        Get slugs of the specific project.
+        """
+        username = os.getenv("BITBUCKET_USERNAME")
+        api_token = os.getenv("BITBUCKET_API_KEY")
+        auth = base64.b64encode(f"{username}:{api_token}".encode()).decode()
 
-      repos_url = f"{self.base_url}/rest/api/latest/projects/{self.project_key}/repos/"
-      headers = {
-          "Authorization": f"Basic {auth}"
-      }
-      slugs = []
-      response = requests.get(repos_url, headers=headers)
+        repos_url = (
+            f"{self.base_url}/rest/api/latest/projects/{self.project_key}/repos/"
+        )
+        headers = {"Authorization": f"Basic {auth}"}
+        slugs = []
+        response = requests.get(repos_url, headers=headers)
 
-      if response.status_code == 200:
-          repositories = response.json()["values"]
-          for repo in repositories:
-              repo_name = repo["name"]
-              repo_slug = repo["slug"]
-              slugs.append(repo_slug)
-      return slugs
-    
-    def load_all_file_paths(self, slug, branch, directory_path='', paths=[]):
-      """
+        if response.status_code == 200:
+            repositories = response.json()["values"]
+            for repo in repositories:
+                repo_name = repo["name"]
+                repo_slug = repo["slug"]
+                slugs.append(repo_slug)
+        return slugs
+
+    def load_all_file_paths(self, slug, branch, directory_path="", paths=[]):
+        """
         Go inside every file that is present in the repository and get the paths for each file
-      """
-      content_url = f"{self.base_url}/rest/api/latest/projects/{self.project_key}/repos/{slug}/browse/{directory_path}"
+        """
+        content_url = f"{self.base_url}/rest/api/latest/projects/{self.project_key}/repos/{slug}/browse/{directory_path}"
 
-      query_params = {
-        'at': branch,
-      }
-      response = requests.get(content_url, headers=headers, params=query_params)
-      children = response.json()['children']
-      for value in children['values']:
-        if (value['type'] == 'FILE'):
-          paths.append({
-              'slug': slug,
-              'path': f'{directory_path}/{value["path"]["name"]}'
-              })
-        elif (value['type'] == 'DIRECTORY'):
-          self.load_all_file_paths(slug=slug, branch=branch, directory_path=f'{directory_path}/{value["path"]["name"]}', paths=paths)
-   
+        query_params = {
+            "at": branch,
+        }
+        response = requests.get(content_url, headers=headers, params=query_params)
+        children = response.json()["children"]
+        for value in children["values"]:
+            if value["type"] == "FILE":
+                paths.append(
+                    {"slug": slug, "path": f'{directory_path}/{value["path"]["name"]}'}
+                )
+            elif value["type"] == "DIRECTORY":
+                self.load_all_file_paths(
+                    slug=slug,
+                    branch=branch,
+                    directory_path=f'{directory_path}/{value["path"]["name"]}',
+                    paths=paths,
+                )
+
     def load_text_by_paths(self, slug, file_path, branch) -> List:
-      """
+        """
         Go inside every file that is present in the repository and get the paths for each file
-      """
-      content_url = f"{self.base_url}/rest/api/latest/projects/{self.project_key}/repos/{slug}/browse{file_path}"
+        """
+        content_url = f"{self.base_url}/rest/api/latest/projects/{self.project_key}/repos/{slug}/browse{file_path}"
 
-      query_params = {
-        'at': branch,
-      }
-      response = requests.get(content_url, headers=headers, params=query_params)
-      children = response.json()
-      return children['lines']
+        query_params = {
+            "at": branch,
+        }
+        response = requests.get(content_url, headers=headers, params=query_params)
+        children = response.json()
+        return children["lines"]
 
+    def load_text(self, paths) -> List:
+        text_dict = []
+        for path in paths:
+            lines_list = load_text_by_paths(
+                self.base_url, self.project_key, path["slug"], path["path"], self.branch
+            )
+            concatenated_string = ""
 
-    def load_text(self, paths) -> List :
-      text_dict = []
-      for path in paths:
+            for line_dict in lines_list:
+                text = line_dict.get("text", "")
+                concatenated_string = concatenated_string + " " + text
 
-        lines_list = load_text_by_paths(self.base_url, self.project_key, path['slug'], path['path'], self.branch)
-        concatenated_string = ""
-
-        for line_dict in lines_list:
-            text = line_dict.get('text', '')
-            concatenated_string = concatenated_string + " " + text
-
-        text_dict.append(concatenated_string)
-      return text_dict
+            text_dict.append(concatenated_string)
+        return text_dict
 
     def load_data(self) -> List[Document]:
         """Return a list of Document made of each file in Bitbucket."""
         slugs = get_slugs()
         paths = []
         for slug in slugs:
-          self.load_all_file_paths(slug=slug, branch=self.branch, directory_path='', paths=paths)
+            self.load_all_file_paths(
+                slug=slug, branch=self.branch, directory_path="", paths=paths
+            )
         texts = self.load_text(paths)
         return [Document(text=text) for text in texts]
