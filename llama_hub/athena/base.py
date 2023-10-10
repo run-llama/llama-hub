63436e0e83b2c40e27f50961a8ac0510670a2656
matichon-vultureprime <matichon.man@vultureprime.com>
2023-10-10 20:58:40 +0700

    already run black to reformat code

--- llama_hub/athena/base.py
+++ llama_hub/athena/base.py
@@ -3,10 +3,11 @@ import warnings
 from typing import Optional
 from llama_index.readers.base import BaseReader
 
-class AthenaReader(BaseReader):
-    """Athena reader. 
 
-    Follow AWS best practices for security. 
+class AthenaReader(BaseReader):
+    """Athena reader.
+
+    Follow AWS best practices for security.
     AWS discourages hardcoding credentials in code.
     We recommend that you use IAM roles instead of IAM user credentials.
     If you must use credentials, do not embed them in your code.
@@ -29,46 +30,55 @@ class AthenaReader(BaseReader):
         workgroup: str = None,
     ):
         """
-            Args:
-            aws_access_key is the AWS access key from aws credential
-            aws_secret_key is the AWS secret key from aws credential
-            aws_region is the AWS region
-            s3_staging_dir is the S3 staging (result bucket) directory
-            database is the Athena database name
-            workgroup is the Athena workgroup name
+        Args:
+        aws_access_key is the AWS access key from aws credential
+        aws_secret_key is the AWS secret key from aws credential
+        aws_region is the AWS region
+        s3_staging_dir is the S3 staging (result bucket) directory
+        database is the Athena database name
+        workgroup is the Athena workgroup name
         """
         import boto3
         from sqlalchemy.engine import create_engine
-        
+
         if not aws_access_key or not aws_secret_key:
 
-            conn_str = "awsathena+rest://:@athena.{region_name}.amazonaws.com:443/"\
+            conn_str = (
+                "awsathena+rest://:@athena.{region_name}.amazonaws.com:443/"
                 "{database}?s3_staging_dir={s3_staging_dir}?work_group={workgroup}"
+            )
 
-            engine = create_engine(conn_str.format(
-                region_name=aws_region,
-                s3_staging_dir=s3_staging_dir,
-                database=database,
-                workgroup=workgroup,
-            ))
+            engine = create_engine(
+                conn_str.format(
+                    region_name=aws_region,
+                    s3_staging_dir=s3_staging_dir,
+                    database=database,
+                    workgroup=workgroup,
+                )
+            )
 
         else:
             warnings.warn(
-                "aws_access_key and aws_secret_key are set. We recommend to use IAM role instead.")
+                "aws_access_key and aws_secret_key are set. We recommend to use IAM role instead."
+            )
             boto3.client(
-                'athena',
+                "athena",
                 aws_access_key_id=aws_access_key,
                 aws_secret_access_key=aws_secret_key,
                 region_name=aws_region,
             )
 
-            conn_str = "awsathena+rest://:@athena.{region_name}.amazonaws.com:443/"\
+            conn_str = (
+                "awsathena+rest://:@athena.{region_name}.amazonaws.com:443/"
                 "{database}?s3_staging_dir={s3_staging_dir}?work_group={workgroup}"
+            )
 
-            engine = create_engine(conn_str.format(
-                region_name=aws_region,
-                s3_staging_dir=s3_staging_dir,
-                database=database,
-                workgroup=workgroup,
-            ))
+            engine = create_engine(
+                conn_str.format(
+                    region_name=aws_region,
+                    s3_staging_dir=s3_staging_dir,
+                    database=database,
+                    workgroup=workgroup,
+                )
+            )
         return engine
