2abd04ae2479b43eece70756b5e7cdfb89ac2a2a
Joshua Ching <joshuakto@gmail.com>
2023-10-23 18:07:44 +0800

    ran make format and fixed lint errors

--- llama_hub/pdb/base.py
+++ llama_hub/pdb/base.py
@@ -1,12 +1,9 @@
 """Simple Reader that reads abstract of primary citation for a given PDB id."""
-import re
-from typing import Any, List, Optional
+from typing import List
 from llama_hub.pdb.utils import get_pdb_abstract
 
 from llama_index.readers.base import BaseReader
 from llama_index.readers.schema.base import Document
-
-from llama_hub.youtube_transcript.utils import YOUTUBE_URL_PATTERNS
 
 
 class PdbAbstractReader(BaseReader):
@@ -15,10 +12,7 @@ class PdbAbstractReader(BaseReader):
     def __init__(self) -> None:
         super().__init__()
 
-    def load_data(
-        self,
-        pdb_ids: List[str]
-    ) -> List[Document]:
+    def load_data(self, pdb_ids: List[str]) -> List[Document]:
         """Load data from RCSB or EBI REST API.
 
         Args:
@@ -30,11 +24,11 @@ class PdbAbstractReader(BaseReader):
         for pdb_id in pdb_ids:
             title, abstracts = get_pdb_abstract(pdb_id)
             primary_citation = abstracts[title]
-            abstract = primary_citation['abstract']
-            results.append(Document(
-                text=abstract, 
-                extra_info={
-                    "pdb_id": pdb_id,
-                    "primary_citation": primary_citation
-                }))
+            abstract = primary_citation["abstract"]
+            results.append(
+                Document(
+                    text=abstract,
+                    extra_info={"pdb_id": pdb_id, "primary_citation": primary_citation},
+                )
+            )
         return results
