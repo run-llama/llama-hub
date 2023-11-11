304297b8aba46f5dd8f0685c88a1de6852ce261d
Emanuel Ferreira <contatoferreirads@gmail.com>
2023-11-11 09:31:25 -0300

    chore: use multi-modal as an example

--- llama_hub/tools/openai/image_generation/base.py
+++ llama_hub/tools/openai/image_generation/base.py
@@ -1,15 +1,23 @@
-"""Azure Speech tool spec."""
+"""OpenAI Image Generation tool sppec.."""
+
+import os
+import base64
+import time
 
 from typing import Optional
 from llama_index.tools.tool_spec.base import BaseToolSpec
 
+from PIL import Image
+from io import BytesIO
+
+DEFAULT_CACHE_DIR = "../../../img_cache"
 
 class OpenAIImageGenerationToolSpec(BaseToolSpec):
     """OpenAI Image Generation tool spec."""
 
     spec_functions = ["image_generation"]
 
-    def __init__(self, api_key: str) -> None:
+    def __init__(self, api_key: str, cache_dir: Optional[str] = None) -> None:
         try:
             from openai import OpenAI
         except ImportError:
@@ -19,6 +27,28 @@ class OpenAIImageGenerationToolSpec(BaseToolSpec):
 
         """Initialize with parameters."""
         self.client = OpenAI(api_key=api_key)
+        self.cache_dir = cache_dir or DEFAULT_CACHE_DIR
+        
+    
+    def get_cache_dir(self):
+        return self.cache_dir
+        
+    def save_base64_image(self, base64_str, image_name):
+        cache_dir = self.cache_dir
+        
+        # Create cache directory if it doesn't exist
+        if not os.path.exists(cache_dir):
+            os.makedirs(cache_dir)
+
+        # Decode the base64 string
+        image_data = base64.b64decode(base64_str)
+
+        # Create an image from the decoded bytes and save it
+        image_path = os.path.join(cache_dir, image_name)
+        with Image.open(BytesIO(image_data)) as img:
+            img.save(image_path)
+
+        return image_path
 
     def image_generation(
         self,
@@ -27,7 +57,6 @@ class OpenAIImageGenerationToolSpec(BaseToolSpec):
         model: Optional[str] = "dall-e-3",
         quality: Optional[str] = "standard",
         num_images: Optional[int] = 1,
-        response_format: Optional[str] = "url",
     ) -> str:
         """
         This tool accepts a natural language string and will use OpenAI's DALL-E model to generate an image.
@@ -38,7 +67,6 @@ class OpenAIImageGenerationToolSpec(BaseToolSpec):
             model (str): The model to use to generate the image (dall-e-3, dall-e-2).
             quality (str): The quality of the image to generate (standard, hd).
             num_images (int): The number of images to generate.
-            response_format (str): The format of the response.
         """
         response = self.client.images.generate(
             model=model,
@@ -46,12 +74,13 @@ class OpenAIImageGenerationToolSpec(BaseToolSpec):
             size=size,
             quality=quality,
             n=num_images,
-            response_format=response_format,
+            response_format="b64_json",
         )
+        
+        image_bytes = response.data[0].b64_json
+        
+        filename = f"{time.time()}.jpg"
+            
+        saved_image_path = self.save_base64_image(image_bytes, filename);
 
-        if response_format == "url":
-            image = response.data[0].url
-        elif response_format == "b64_json":
-            image = response.data[0].b64_json
-
-        return image
+        return saved_image_path
