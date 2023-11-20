dfe36b10e0140ddc96471166bb398082e0f6a8b3
Logan Markewich <logan.markewich@live.com>
2023-11-20 17:35:04 -0600

    linting

--- llama_hub/llama_packs/resume_screener/base.py
+++ llama_hub/llama_packs/resume_screener/base.py
@@ -21,17 +21,26 @@ You job is to decide if the candidate pass the resume screen given the job descr
 {criteria_str}
 """
 
+
 class CriteriaDecision(BaseModel):
     """The decision made based on a single criteria"""
+
     decision: bool = Field(description="The decision made based on the criteria")
     reasoning: str = Field(description="The reasoning behind the decision")
 
 
 class ResumeScreenerDecision(BaseModel):
     """The decision made by the resume screener"""
-    criteria_decisions: List[CriteriaDecision] = Field( description="The decisions made based on the criteria")
-    overall_reasoning: str = Field(description="The reasoning behind the overall decision")
-    overall_decision: bool = Field(description="The overall decision made based on the criteria")
+
+    criteria_decisions: List[CriteriaDecision] = Field(
+        description="The decisions made based on the criteria"
+    )
+    overall_reasoning: str = Field(
+        description="The reasoning behind the overall decision"
+    )
+    overall_decision: bool = Field(
+        description="The overall decision made based on the criteria"
+    )
 
 
 def _format_criteria_str(criteria: List[str]) -> str:
@@ -40,22 +49,26 @@ def _format_criteria_str(criteria: List[str]) -> str:
         criteria_str += f"- {criterion}\n"
     return criteria_str
 
+
 class ResumeScreenerPack(BaseLlamaPack):
-    def __init__(self, job_description: str, criteria: List[str], llm: Optional[LLM] = None) -> None:
+    def __init__(
+        self, job_description: str, criteria: List[str], llm: Optional[LLM] = None
+    ) -> None:
         self.reader = PDFReader()
-        llm = llm or OpenAI(model='gpt-4')
+        llm = llm or OpenAI(model="gpt-4")
         service_context = ServiceContext.from_defaults(llm=llm)
-        self.synthesizer = TreeSummarize(output_cls=ResumeScreenerDecision, service_context=service_context)
+        self.synthesizer = TreeSummarize(
+            output_cls=ResumeScreenerDecision, service_context=service_context
+        )
         criteria_str = _format_criteria_str(criteria)
-        self.query = QUERY_TEMPLATE.format(job_description=job_description, criteria_str=criteria_str)
+        self.query = QUERY_TEMPLATE.format(
+            job_description=job_description, criteria_str=criteria_str
+        )
 
     def get_modules(self) -> Dict[str, Any]:
         """Get modules."""
-        return {
-            "reader": self.reader,
-            "synthesizer": self.synthesizer
-        }
-    
+        return {"reader": self.reader, "synthesizer": self.synthesizer}
+
     def run(self, resume_path: str, *args: Any, **kwargs: Any) -> Any:
         """Run pack."""
         docs = self.reader.load_data(Path(resume_path))
@@ -64,5 +77,3 @@ class ResumeScreenerPack(BaseLlamaPack):
             nodes=[NodeWithScore(node=doc, score=1.0) for doc in docs],
         )
         return output.response
-    
-
