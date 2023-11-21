c75cf7c1dfbba26a9469aeb50978cf8397f5a760
Andrei Fajardo <andrei@nerdai.io>
2023-11-21 00:07:21 -0500

    wip

--- llama_hub/llama_packs/gradio_chatbot/base.py
+++ llama_hub/llama_packs/gradio_chatbot/base.py
@@ -11,9 +11,21 @@ from llama_index import ServiceContext
 from llama_index.readers import WikipediaReader
 from llama_index.agent import ReActAgent
 from llama_index.tools import QueryEngineTool, ToolMetadata
+from ansi2html import Ansi2HTMLConverter
 
 from contextlib import redirect_stdout
-import io
+from io import StringIO 
+import sys
+
+class Capturing(list):
+    def __enter__(self):
+        self._stdout = sys.stdout
+        sys.stdout = self._stringio = StringIO()
+        return self
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        del self._stringio    # free up some memory
+        sys.stdout = self._stdout
 
 
 class GradioChatPack(BaseLlamaPack):
@@ -43,6 +55,8 @@ class GradioChatPack(BaseLlamaPack):
             )
         )]
         self.agent = ReActAgent.from_tools(tools, llm=self.llm, verbose=True)
+        self.thoughts = ""
+        self.conv = Ansi2HTMLConverter()
 
 
     def get_modules(self) -> Dict[str, Any]:
@@ -53,31 +67,32 @@ class GradioChatPack(BaseLlamaPack):
         return "", history + [(user_message, "")]
     
     def _generate_response(self, chat_history: List[Tuple[str, str]]) -> Tuple[str, List[Tuple[str,str]]]:
-        f = io.StringIO()
-        with redirect_stdout(f):
+        with Capturing() as output:
             response = self.agent.stream_chat(chat_history[-1][0])
+        ansi = "\n========\n".join(output)
+        html_output = self.conv.convert(ansi)
+        print(html_output)
         for token in response.response_gen:
-            print(token)
             chat_history[-1][1] += token
-            yield chat_history
+            yield chat_history, str(html_output)
 
     def run(self, *args: Any, **kwargs: Any) -> Any:
         """Run the pipeline."""
         import gradio as gr
 
 
-        demo = gr.Blocks() 
+        demo = gr.Blocks(css="#box { height: 420px; overflow-y: scroll !important}") 
         with demo:
             gr.Markdown("ReAct Agent")
             with gr.Row():
                 chat_window = gr.Chatbot(label="Message History", scale=3)
-                console = gr.Textbox(label="Thoughts", lines=18, interactive=False)
+                console = gr.HTML(label="Thoughts", elem_id="box")
             with gr.Row():
                 message = gr.Textbox(label="Write A Message", scale=4)
                 clear = gr.ClearButton([message, chat_window])
             
             message.submit(self._user, [message, chat_window], [message, chat_window], queue=False).then(
-                self._generate_response, chat_window, chat_window
+                self._generate_response, chat_window, [chat_window, console], queue=True
             )
         demo.launch(server_name="0.0.0.0", server_port=8080)
 
