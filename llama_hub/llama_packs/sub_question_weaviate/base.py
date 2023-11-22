ea4d6ab6a6419aec331023a52dc88f8444e8469c
Logan Markewich <logan.markewich@live.com>
2023-11-21 20:05:46 -0600

    unused import

--- llama_hub/llama_packs/sub_question_weaviate/base.py
+++ llama_hub/llama_packs/sub_question_weaviate/base.py
@@ -8,7 +8,6 @@ from llama_index.llama_pack.base import BaseLlamaPack
 from llama_index.schema import TextNode
 from llama_index.storage.storage_context import StorageContext
 from llama_index.vector_stores.weaviate import WeaviateVectorStore
-from llama_index.vector_stores.types import VectorStoreInfo
 from llama_index.tools import QueryEngineTool, ToolMetadata
 from llama_index.query_engine import SubQuestionQueryEngine
 
