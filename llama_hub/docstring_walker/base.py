5f5849448680921c9e96fbc1e5cb6edd9c0d1589
fiwojcik <filip.wojcik@outlook.com>
2023-12-01 11:43:58 +0100

    Linted code for DocstringWalker

--- llama_hub/docstring_walker/base.py
+++ llama_hub/docstring_walker/base.py
@@ -1,41 +1,47 @@
+"""Main module for DocstringWalker loader for Llama Hub"""
+
+
 import ast
 import os
 import logging
-
+from typing import List
 from llama_index import Document
 from llama_index.readers.base import BaseReader
-from typing import Any, Tuple, List
+
 
 TYPES_TO_PROCESS = {ast.FunctionDef, ast.ClassDef}
-
-
-from typing import List, Tuple
 
 log = logging.getLogger(__name__)
 
 
 class DocstringWalker(BaseReader):
     """A loader for docstring extraction and building structured documents from them.
-    Recursively walks a directory and extracts docstrings from each Python module - starting from the module
-    itself, then classes, then functions. Builds a graph of dependencies between the extracted docstrings.
+    Recursively walks a directory and extracts docstrings from each Python
+    module - starting from the module itself, then classes, then functions.
+    Builds a graph of dependencies between the extracted docstrings.
     """
-      
-    def load_data(self, code_dir: str, skip_initpy: bool = True, fail_on_malformed_files: bool = False) -> List[Document]:
+
+    def load_data(
+        self,
+        code_dir: str,
+        skip_initpy: bool = True,
+        fail_on_malformed_files: bool = False,
+    ) -> List[Document]:
         """
         Load data from the specified code directory.
         Additionally, after loading the data, build a dependency graph between the loaded documents.
         The graph is stored as an attribute of the class.
-        
+
 
         Parameters
         ----------
-        code_dir : str 
+        code_dir : str
             The directory path to the code files.
         skip_initpy : bool
             Whether to skip the __init__.py files. Defaults to True.
         fail_on_malformed_files : bool
-            Whether to fail on malformed files. Defaults to False - in this case, the malformed files are skipped
-            and a warning is logged.
+            Whether to fail on malformed files. Defaults to False - in this case,
+            the malformed files are skipped and a warning is logged.
 
         Returns
         -------
@@ -43,30 +49,34 @@ class DocstringWalker(BaseReader):
             A list of loaded documents.
         """
 
-        llama_docs = self.process_directory(code_dir, skip_initpy, fail_on_malformed_files)
+        llama_docs = self.process_directory(
+            code_dir, skip_initpy, fail_on_malformed_files
+        )
         return llama_docs
-    
-   
-    def process_directory(self, code_dir: str, skip_initpy: bool = True, fail_on_malformed_files: bool = False) -> List[Document]:
+
+    def process_directory(
+        self,
+        code_dir: str,
+        skip_initpy: bool = True,
+        fail_on_malformed_files: bool = False,
+    ) -> List[Document]:
         """
         Process a directory and extract information from Python files.
         Parameters
         ----------
-        code_dir : str 
+        code_dir : str
             The directory path to the code files.
         skip_initpy : bool
             Whether to skip the __init__.py files. Defaults to True.
         fail_on_malformed_files : bool
-            Whether to fail on malformed files. Defaults to False - in this case, the malformed files are skipped
-            and a warning is logged.
-            
+            Whether to fail on malformed files. Defaults to False - in this case,
+            the malformed files are skipped and a warning is logged.
+
         Returns
         -------
-        Tuple[List[Document], nx.Graph]
-            A tuple containing a list of Document objects and a networkx Graph object.
-            The Document objects represent the extracted information from Python files in the directory.
-            The Graph object represents the dependency graph between the extracted documents.
-        """    
+        List[Document]
+            A list of Document objects.
+        """
         llama_docs = []
         for root, _, files in os.walk(code_dir):
             for file in files:
@@ -81,11 +91,12 @@ class DocstringWalker(BaseReader):
                     except Exception as e:
                         if fail_on_malformed_files:
                             raise e
-                        else:
-                            log.warning(f"Failed to parse file {module_path}. Skipping. Error: {e}")
-                            continue
+                        log.warning(
+                            "Failed to parse file %s. Skipping. Error: %s",
+                            module_path,
+                            e)
+                        continue
         return llama_docs
-
 
     def read_module_text(self, path: str) -> str:
         """Read the text of a Python module. For tests this function can be mocked.
@@ -100,10 +111,9 @@ class DocstringWalker(BaseReader):
         str
             The text of the module.
         """
-        with open(path) as f:
+        with open(path, mode='r', encoding="utf-8") as f:
             text = f.read()
         return text
-
 
     def parse_module(self, module_name: str, path: str) -> Document:
         """Function for parsing a single Python module.
@@ -133,7 +143,6 @@ class DocstringWalker(BaseReader):
         document = Document(text=module_text)
         return document
 
-
     def process_class(self, class_node: ast.ClassDef, parent_node: str):
         """
         Process a class node in the AST and add relevant information to the graph.
@@ -141,20 +150,21 @@ class DocstringWalker(BaseReader):
         Parameters:
         ----------
         class_node : ast.ClassDef
-            The class node to process. It represents a class definition in the abstract syntax tree (AST).
-        parent_node : str 
+            The class node to process. It represents a class definition
+            in the abstract syntax tree (AST).
+        parent_node : str
             The name of the parent node. It specifies the name of the parent node in the graph.
 
         Returns:
         ----------
-        str 
-            A string representation of the processed class node and its sub-elements. 
+        str
+            A string representation of the processed class node and its sub-elements.
             It provides a textual representation of the processed class node and its sub-elements.
         """
         cls_name = class_node.name
         cls_docstring = ast.get_docstring(class_node)
 
-        text = f"\n Class name: {cls_name} \n Docstring: {cls_docstring}"
+        text = f"\n Class name: {cls_name}, In: {parent_node} \n Docstring: {cls_docstring}"
         sub_texts = []
         for elem in class_node.body:
             sub_text = self.process_elem(elem, cls_name)
@@ -185,14 +195,14 @@ class DocstringWalker(BaseReader):
         for elem in func_node.body:
             sub_text = self.process_elem(elem, func_name)
             sub_texts.append(sub_text)
-        return text+ "\n".join(sub_texts)
-
+        return text + "\n".join(sub_texts)
 
     def process_elem(self, elem, parent_node: str) -> str:
         """
         Process an element in the abstract syntax tree (AST).
 
-        This is a generic function that delegates the execution to more specific functions based on the type of the element.
+        This is a generic function that delegates the execution to more specific
+        functions based on the type of the element.
 
         Args:
             elem (ast.AST): The element to process.
@@ -202,9 +212,9 @@ class DocstringWalker(BaseReader):
         Returns:
             str: The result of processing the element.
         """
-        if type(elem) == ast.FunctionDef:
+
+        if isinstance(elem, ast.FunctionDef):
             return self.process_function(elem, parent_node)
-        elif type(elem) == ast.ClassDef:
+        elif isinstance(elem, ast.ClassDef):
             return self.process_class(elem, parent_node)
-        else:
-            return ""
+        return ""
