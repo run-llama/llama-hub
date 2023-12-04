9609b39d0d1e0c7edd478dc2c33eaee0ab7c106f
Andrei Fajardo <andrei@nerdai.io>
2023-12-04 00:58:02 -0500

    fmt/lint

--- llama_hub/llama_datasets/llama2_paper/llamaindex_baseline.py
+++ llama_hub/llama_datasets/llama2_paper/llamaindex_baseline.py
@@ -7,9 +7,7 @@ from llama_index import VectorStoreIndex
 
 async def main():
     # DOWNLOAD LLAMADATASET
-    rag_dataset, documents = download_llama_dataset(
-        "Llama2PaperDataset", "./data"
-    )
+    rag_dataset, documents = download_llama_dataset("Llama2PaperDataset", "./data")
 
     # BUILD BASIC RAG PIPELINE
     index = VectorStoreIndex.from_documents(documents=documents)
