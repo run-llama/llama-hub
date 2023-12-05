b547cfbb5a33e19929bf1abfd6240acdbf5392ee
wenqiglantz <wenqiglantz@gmail.com>
2023-12-04 22:41:13 -0500

    fix test issue

--- llama_hub/llama_packs/snowflake_query_engine/README.md
+++ llama_hub/llama_packs/snowflake_query_engine/README.md
@@ -17,27 +17,20 @@ SnowflakeQueryEnginePack = download_llama_pack(
 
 From here, you can use the pack, or inspect and modify the pack in `./snowflake_pack`.
 
-First, add the Snowflake DB connection details in a file named `credentials.json` at your project root, the same directory where your main python file is located. See below the sample file content, replace the placeholder values with your actual Snowflake DB connection details.
-
-```
-{
-  "account": "<YOUR-ORG>-<YOUR-ACCOUNT>",
-  "user": "<YOUR-USER-NAME>",
-  "role": "<USER-ROLE>",
-  "password": "<USER-PASSWORD>",
-  "warehouse": "<WAREHOUSE>",
-  "database": "<DATABASE>",
-  "schema": "<SCHEMA>"
-}
-
-```
 `NLSQLTableQueryEngine` uses OpenAI models by default, ensure you set your OpenAI API key.
 
-Then, you can set up the pack by passing in the table(s):
+You can set up the pack by passing in the Snowflake connection details and the table(s):
 
 ```python
 # create the pack
 snowflake_pack = SnowflakeQueryEnginePack(
+  user="<USER>",
+  password="<PASSWORD>",
+  account="<ORG>-<ACCOUNT>",
+  database="<DATABASE>",
+  schema="<SCHEMA>",
+  warehouse="<WAREHOUSE>",
+  role="<ROLE>",
   tables=["<TABLE1>", "<TABLE2>",...]
 )
 ```
--- llama_hub/llama_packs/snowflake_query_engine/base.py
+++ llama_hub/llama_packs/snowflake_query_engine/base.py
@@ -1,39 +1,7 @@
 """Snowflake Query Engine Pack."""
 
 import os
-import json
 from typing import Any, Dict, List
-
-
-def snowflake_sqlalchemy_20_monkey_patches():
-    import sqlalchemy.util.compat
-
-    # make strings always return unicode strings
-    sqlalchemy.util.compat.string_types = (str,)
-    sqlalchemy.types.String.RETURNS_UNICODE = True
-
-    import snowflake.sqlalchemy.snowdialect
-
-    snowflake.sqlalchemy.snowdialect.SnowflakeDialect.returns_unicode_strings = True
-
-    # make has_table() support the `info_cache` kwarg
-    import snowflake.sqlalchemy.snowdialect
-
-    def has_table(self, connection, table_name, schema=None, info_cache=None):
-        """
-        Checks if the table exists
-        """
-        return self._has_object(connection, "TABLE", table_name, schema)
-
-    snowflake.sqlalchemy.snowdialect.SnowflakeDialect.has_table = has_table
-
-
-# workaround for https://github.com/snowflakedb/snowflake-sqlalchemy/issues/380.
-try:
-    snowflake_sqlalchemy_20_monkey_patches()
-except Exception:
-    raise ImportError("Please run `pip install snowflake-sqlalchemy`")
-
 from sqlalchemy import create_engine
 from llama_index import SQLDatabase, ServiceContext
 from llama_index.llama_pack.base import BaseLlamaPack
@@ -50,25 +18,28 @@ class SnowflakeQueryEnginePack(BaseLlamaPack):
 
     def __init__(
         self,
+        user: str,
+        password: str,
+        account: str,
+        database: str,
+        schema: str,
+        warehouse: str,
+        role: str,
         tables: List[str],
         **kwargs: Any,
     ) -> None:
         """Init params."""
 
+        # workaround for https://github.com/snowflakedb/snowflake-sqlalchemy/issues/380.
+        try:
+            snowflake_sqlalchemy_20_monkey_patches()
+        except Exception:
+            raise ImportError("Please run `pip install snowflake-sqlalchemy`")
+
         if not os.environ.get("OPENAI_API_KEY", None):
             raise ValueError("OpenAI API Token is missing or blank.")
 
-        # Check if credentials file exists
-        if not os.path.exists("credentials.json"):
-            raise FileNotFoundError(
-                "The 'credentials.json' file is mandatory for connecting to Snowflake."
-            )
-
-        # connect to Snowflake
-        with open("credentials.json") as f:
-            connection_parameters = json.load(f)
-
-        snowflake_uri = f"snowflake://{connection_parameters['user']}:{connection_parameters['password']}@{connection_parameters['account']}/{connection_parameters['database']}/{connection_parameters['schema']}?warehouse={connection_parameters['warehouse']}&role={connection_parameters['role']}"
+        snowflake_uri = f"snowflake://{user}:{password}@{account}/{database}/{schema}?warehouse={warehouse}&role={role}"
 
         engine = create_engine(snowflake_uri)
 
@@ -94,3 +65,26 @@ class SnowflakeQueryEnginePack(BaseLlamaPack):
     def run(self, *args: Any, **kwargs: Any) -> Any:
         """Run the pipeline."""
         return self.query_engine.query(*args, **kwargs)
+
+
+def snowflake_sqlalchemy_20_monkey_patches():
+    import sqlalchemy.util.compat
+
+    # make strings always return unicode strings
+    sqlalchemy.util.compat.string_types = (str,)
+    sqlalchemy.types.String.RETURNS_UNICODE = True
+
+    import snowflake.sqlalchemy.snowdialect
+
+    snowflake.sqlalchemy.snowdialect.SnowflakeDialect.returns_unicode_strings = True
+
+    # make has_table() support the `info_cache` kwarg
+    import snowflake.sqlalchemy.snowdialect
+
+    def has_table(self, connection, table_name, schema=None, info_cache=None):
+        """
+        Checks if the table exists
+        """
+        return self._has_object(connection, "TABLE", table_name, schema)
+
+    snowflake.sqlalchemy.snowdialect.SnowflakeDialect.has_table = has_table
