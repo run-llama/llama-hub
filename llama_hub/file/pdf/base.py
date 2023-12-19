9a9fa5b969cca2a9bcf8b97c6e74bfaa27eb8fc7
JAlexMcGraw <jamcgraw21@gmail.com>
2023-12-19 16:19:17 -0600

    Update PDFReader
    Added to the class the ability to use PDFPlumber as the underlying library to extract text. From my own testing, it performs better than PyPDF, albeit a little slower.

--- llama_hub/file/pdf/base.py
+++ llama_hub/file/pdf/base.py
@@ -1,45 +1,95 @@
 """Read PDF files."""
 
 from pathlib import Path
-from typing import IO, Dict, List, Optional, Union
+from typing import Dict, List, Optional
 
 from llama_index.readers.base import BaseReader
 from llama_index.readers.schema.base import Document
 
 
+from pathlib import Path
+from typing import Dict, List, Optional
+
+from llama_index.readers.base import BaseReader
+from llama_index.schema import Document
+
+
 class PDFReader(BaseReader):
-    """PDF reader."""
+    """PDF parser."""
+
+    def __init__(
+        self,
+        return_full_document: Optional[bool] = False,
+        use_pdfplumber: Optional[bool] = False,
+    ) -> None:
+        """
+        Initialize PDFReader.
+        """
+        self.return_full_document = return_full_document
+        self.use_pdfplumber = use_pdfplumber
 
     def load_data(
-        self, file: Union[IO[bytes], str, Path], extra_info: Optional[Dict] = None
+        self, file: Path, extra_info: Optional[Dict] = None
     ) -> List[Document]:
         """Parse file."""
-        import pypdf
 
-        # Check if the file is already a Path object, if not, create a Path object from the string
-        if not isinstance(file, Path) and isinstance(file, str):
-            file = Path(file)
+        docs = []
 
-        # Open the file if it's not already open, else use it as it is
-        context = open(file, "rb") if isinstance(file, Path) else file
+        if self.use_pdfplumber:
+            # TODO
+            try:
+                import pdfplumber
+            except ImportError:
+                raise ImportError(
+                    "pdfplumber is required to read PDF files: `pip install pdfplumber`"
+                )
+            with pdfplumber.open(file) as fp:
+                text_list = [page.extract_text() for page in fp.pages]
+                text = "\n".join(text_list)
+                metadata = {"file_name": fp.stream.name.split("/")[-1]}
 
-        with context as fp:
-            # Create a PDF object
-            pdf = pypdf.PdfReader(fp)
+                docs.append(Document(text=text, metadata=metadata))
+                fp.close()
 
-            # Get the number of pages in the PDF document
-            num_pages = len(pdf.pages)
+        else:
+            try:
+                import pypdf
+            except ImportError:
+                raise ImportError(
+                    "pypdf is required to read PDF files: `pip install pypdf`"
+                )
+            with open(file, "rb") as fp:
+                # Create a PDF object
+                pdf = pypdf.PdfReader(fp)
 
-            # Iterate over every page
-            docs = []
-            for page in range(num_pages):
-                # Extract the text from the page
-                page_text = pdf.pages[page].extract_text()
-                page_label = pdf.page_labels[page]
-                metadata = {"page_label": page_label, "file_name": file.name}
+                # Get the number of pages in the PDF document
+                num_pages = len(pdf.pages)
 
-                if extra_info is not None:
-                    metadata.update(extra_info)
+                # This block returns a whole PDF as a single Document
+                if self.return_full_document:
+                    text = ""
+                    metadata = {"file_name": fp.name}
 
-                docs.append(Document(text=page_text, extra_info=metadata))
-            return docs
+                    for page in range(num_pages):
+                        # Extract the text from the page
+                        page_text = pdf.pages[page].extract_text()
+                        text += page_text
+
+                    docs.append(Document(text=text, metadata=metadata))
+
+                # This block returns each page of a PDF as its own Document
+                else:
+                    # Iterate over every page
+
+                    for page in range(num_pages):
+                        # Extract the text from the page
+                        page_text = pdf.pages[page].extract_text()
+                        page_label = pdf.page_labels[page]
+
+                        metadata = {"page_label": page_label, "file_name": fp.name}
+                        if extra_info is not None:
+                            metadata.update(extra_info)
+
+                        docs.append(Document(text=page_text, metadata=metadata))
+
+        return docs
