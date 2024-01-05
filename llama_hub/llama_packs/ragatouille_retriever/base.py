9cdec8b86b5775c7f2f8baeb8a4cc9af336962d2
Jerry Liu <jerryjliu98@gmail.com>
2024-01-04 16:04:56 -0800

    cr

--- llama_hub/llama_packs/ragatouille_retriever/base.py
+++ llama_hub/llama_packs/ragatouille_retriever/base.py
@@ -3,10 +3,10 @@
 
 from typing import Any, Dict, List, Optional
 
-from llama_index.schema import Document, NodeWithScore
+from llama_index.schema import Document, NodeWithScore, TextNode
 
 from llama_index.llama_pack.base import BaseLlamaPack
-from llama_index.llms import Replicate, OpenAI
+from llama_index.llms import OpenAI
 from llama_index.llms.llm import LLM
 from llama_index.query_engine import RetrieverQueryEngine
 from llama_index.retrievers import BaseRetriever
@@ -16,19 +16,34 @@ from llama_index.indices.query.schema import QueryBundle
 
 class CustomRetriever(BaseRetriever):
     """Custom retriever."""
-    
-    def __init__(self, rag_obj: Any, index_name: str, **kwargs: Any) -> None:
+
+    def __init__(
+        self, rag_obj: Any, index_name: str, top_k: int = 10, **kwargs: Any
+    ) -> None:
         """Init params."""
         try:
-            import ragatouille
+            import ragatouille  # noqa
         except ImportError:
-            raise ValueError("RAGatouille is not installed. Please install it with `pip install ragatouille`.")
+            raise ValueError(
+                "RAGatouille is not installed. Please install it with `pip install ragatouille`."
+            )
         self.rag_obj = rag_obj
         self.index_name = index_name
+        self.top_k = top_k
 
     def _retrieve(self, query_bundle: QueryBundle) -> List[NodeWithScore]:
         """Retrieve."""
-        return self.rag_obj.search(query_bundle.query_str, index_name=self.index_name)
+        results = self.rag_obj.search(
+            query_bundle.query_str, index_name=self.index_name, k=self.top_k
+        )
+        result_nodes = []
+        for result in results:
+            result_nodes.append(
+                NodeWithScore(
+                    node=TextNode(text=result["content"]), score=result["score"]
+                )
+            )
+        return result_nodes
 
 
 class RAGatouilleRetrieverPack(BaseLlamaPack):
@@ -41,15 +56,17 @@ class RAGatouilleRetrieverPack(BaseLlamaPack):
         index_name: str = "my_index",
         llm: Optional[LLM] = None,
         index_path: Optional[str] = None,
+        top_k: int = 10,
     ) -> None:
         """Init params."""
         self._model_name = model_name
         try:
-            import ragatouille
+            import ragatouille  # noqa
             from ragatouille import RAGPretrainedModel
         except ImportError:
-            raise ValueError("RAGatouille is not installed. Please install it with `pip install ragatouille`.")
-
+            raise ValueError(
+                "RAGatouille is not installed. Please install it with `pip install ragatouille`."
+            )
 
         doc_txts = [doc.get_content() for doc in documents]
 
@@ -60,15 +77,16 @@ class RAGatouilleRetrieverPack(BaseLlamaPack):
         else:
             RAG = RAGPretrainedModel.from_index(index_path)
 
-        self.custom_retriever = CustomRetriever(RAG, index_name=index_name)
+        self.index_path = index_path
+
+        self.custom_retriever = CustomRetriever(RAG, index_name=index_name, top_k=top_k)
 
         self.RAG = RAG
         self.documents = documents
 
         self.llm = llm or OpenAI(model="gpt-3.5-turbo")
         self.query_engine = RetrieverQueryEngine.from_args(
-            self.custom_retriever,
-            service_context=ServiceContext.from_defaults(llm=llm)
+            self.custom_retriever, service_context=ServiceContext.from_defaults(llm=llm)
         )
 
     def get_modules(self) -> Dict[str, Any]:
@@ -79,6 +97,7 @@ class RAGatouilleRetrieverPack(BaseLlamaPack):
             "retriever": self.custom_retriever,
             "llm": self.llm,
             "query_engine": self.query_engine,
+            "index_path": self.index_path,
         }
 
     def run(self, *args: Any, **kwargs: Any) -> Any:
