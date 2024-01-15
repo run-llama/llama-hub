681877198d1d95be03c58ea4f78ce180a99fae5f
Ravi Theja <ravi03071991@gmail.com>
2024-01-15 21:56:12 +0530

    Update MultiTenancyRAGPack (#863)
    * Update MultiTenancyRAGPack
    
    * Update IngestionPipeline with num_workers

--- llama_hub/llama_packs/multi_tenancy_rag/base.py
+++ llama_hub/llama_packs/multi_tenancy_rag/base.py
@@ -1,9 +1,13 @@
 from typing import Any, Dict, List
-from llama_index import ServiceContext, VectorStoreIndex
+from llama_index import ServiceContext, VectorStoreIndex, get_response_synthesizer
 from llama_index.llms import OpenAI
 from llama_index.llama_pack.base import BaseLlamaPack
 from llama_index.schema import Document
 from llama_index.vector_stores.types import MetadataFilters, ExactMatchFilter
+from llama_index.retrievers import VectorIndexRetriever
+from llama_index.query_engine import RetrieverQueryEngine
+from llama_index.ingestion import IngestionPipeline
+from llama_index.text_splitter import SentenceSplitter
 
 
 class MultiTenancyRAGPack(BaseLlamaPack):
@@ -21,13 +25,24 @@ class MultiTenancyRAGPack(BaseLlamaPack):
 
     def add(self, documents: List[Document], user: Any) -> None:
         """Insert Documents of a user into index"""
+        # Add metadata to documents
         for document in documents:
             document.metadata["user"] = user
-            self.index.insert(document)
+        # Create Nodes using IngestionPipeline
+        pipeline = IngestionPipeline(
+            transformations=[
+                SentenceSplitter(chunk_size=512, chunk_overlap=20),
+            ]
+        )
+        nodes = pipeline.run(documents=documents, num_workers=4)
+        # Insert nodes into the index
+        self.index.insert_nodes(nodes)
 
     def run(self, query_str: str, user: Any, **kwargs: Any) -> Any:
         """Run the pipeline."""
-        query_engine = self.index.as_query_engine(
+        # Define retriever to filter out nodes for user and query
+        retriever = VectorIndexRetriever(
+            index=self.index,
             filters=MetadataFilters(
                 filters=[
                     ExactMatchFilter(
@@ -37,5 +52,11 @@ class MultiTenancyRAGPack(BaseLlamaPack):
                 ]
             ),
             **kwargs
+        )
+        # Define response synthesizer
+        response_synthesizer = get_response_synthesizer(response_mode="compact")
+        # Define Query Engine
+        query_engine = RetrieverQueryEngine(
+            retriever=retriever, response_synthesizer=response_synthesizer
         )
         return query_engine.query(query_str)
