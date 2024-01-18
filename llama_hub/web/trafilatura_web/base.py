91ea405213e60da7c89795e867fd765f63cd8920
Marcel Reschke <hello@marcelreschke.com>
2024-01-18 20:54:10 +0100

    Reformating base.py

--- llama_hub/web/trafilatura_web/base.py
+++ llama_hub/web/trafilatura_web/base.py
@@ -20,7 +20,16 @@ class TrafilaturaWebReader(BaseReader):
                 "Please `pip install trafilatura` to use this Reader"
             )
 
-    def load_data(self, urls: List[str], include_comments=True, output_format='txt', include_tables=True, include_images=False, include_formatting=False, include_links=False) -> List[Document]:
+    def load_data(
+        self,
+        urls: List[str],
+        include_comments=True,
+        output_format="txt",
+        include_tables=True,
+        include_images=False,
+        include_formatting=False,
+        include_links=False,
+    ) -> List[Document]:
         """Load data from the urls.
 
         Args:
@@ -43,7 +52,15 @@ class TrafilaturaWebReader(BaseReader):
         documents = []
         for url in urls:
             downloaded = trafilatura.fetch_url(url)
-            response = trafilatura.extract(downloaded, include_comments=include_comments, output_format=output_format, include_tables=include_tables, include_images=include_images, include_formatting=include_formatting, include_links=include_links)
+            response = trafilatura.extract(
+                downloaded,
+                include_comments=include_comments,
+                output_format=output_format,
+                include_tables=include_tables,
+                include_images=include_images,
+                include_formatting=include_formatting,
+                include_links=include_links,
+            )
             documents.append(Document(text=response))
 
         return documents
