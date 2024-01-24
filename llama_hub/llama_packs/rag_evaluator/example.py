666cb174337c1f11230ec797c6b7202e6906aa78
thucpn <thucsh2@gmail.com>
2024-01-24 16:19:26 +0700

    lint: using black

--- llama_hub/llama_packs/rag_evaluator/example.py
+++ llama_hub/llama_packs/rag_evaluator/example.py
@@ -15,15 +15,12 @@ query_engine = index.as_query_engine()
 
 # Time to benchmark/evaluate this RAG pipeline
 # Download and install dependencies
-RagEvaluatorPack = download_llama_pack(
-  "RagEvaluatorPack", "./rag_evaluator_pack"
-)
+RagEvaluatorPack = download_llama_pack("RagEvaluatorPack", "./rag_evaluator_pack")
 
 # construction requires a query_engine, a rag_dataset, and optionally a judge_llm
 rag_evaluator_pack = RagEvaluatorPack(
-    query_engine=query_engine,
-    rag_dataset=rag_dataset
-) 
+    query_engine=query_engine, rag_dataset=rag_dataset
+)
 
 # PERFORM EVALUATION
 benchmark_df = rag_evaluator_pack.run()  # async arun() also supported
